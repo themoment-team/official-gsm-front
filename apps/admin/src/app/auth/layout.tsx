7d4aac8412004af45107425dd2707d7d55ceecc6
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-17 01:30:15 +0900

    Add auth layout

---
+++ apps/admin/src/app/auth/layout.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Background>
+      <Content>{children}</Content>
+    </Background>
+  );
+}
+
+const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Content = styled.div`
+  width: 23.438rem;
+  height: 39.875rem;
+  border-radius: 1.25rem;
+  background-color: #ffffff;
+`;
---
+++ apps/admin/src/app/auth/signin/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function SigninPage() {
+  return <div>signin</div>;
+}
