409f018384ce094128f97c6ec9127d26e74e2fa7
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-17 22:07:43 +0900

    Add ToBackButton

---
+++ apps/admin/src/components/ToBackButton/index.tsx
@@ -0,0 +1,13 @@
+import { ToBackIcon } from "assets";
+import React from "react";
+import { Button } from "./style";
+
+const ToBackButton: React.FC = ({}) => {
+  return (
+    <Button>
+      <ToBackIcon />
+    </Button>
+  );
+};
+
+export default ToBackButton;
---
+++ apps/admin/src/components/ToBackButton/style.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.button`
+  position: relative;
+  left: 1.375rem;
+  top: 1.75rem;
+`;
