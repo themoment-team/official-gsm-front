792804696cf861294997865b700a533fff05ecf9
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-17 14:00:14 +0900

    Add Button

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -1,11 +1,30 @@
+import { css } from "@emotion/react";
 import * as S from "./style";
 
 interface ButtonProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  disabled?: boolean;
+  isLoading?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <S.Button>{children}</S.Button>;
+const Button: React.FC<ButtonProps> = ({
+  children = "",
+  disabled = false,
+  isLoading = false,
+}) => {
+  return (
+    <S.Button
+      css={
+        disabled &&
+        css`
+          background: #dcdcdc;
+          pointer-events: none;
+        `
+      }
+    >
+      {isLoading ? "" : children}
+    </S.Button>
+  );
 };
 
 export default Button;
