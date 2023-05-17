790f126d54b6201d8af2917d514d223efaba0c15
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-17 16:21:53 +0900

    Add LoadingCircle in Button

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -1,25 +1,29 @@
 import { css } from "@emotion/react";
 import * as S from "./style";
+import LoadingCircle from "../LoadingCircle";
 
 interface ButtonProps {
   children?: React.ReactNode;
   disabled?: boolean;
   isLoading?: boolean;
+  widthREM?: number;
 }
 
 const Button: React.FC<ButtonProps> = ({
   children = "",
   disabled = false,
   isLoading = false,
+  widthREM,
 }) => {
   return (
     <S.Button
       css={css`
         background: ${disabled && "#dcdcdc"};
         pointer-events: ${(disabled || isLoading) && "none"};
+        width: ${widthREM && `${widthREM}rem`};
       `}
     >
-      {isLoading ? "" : children}
+      {isLoading ? <LoadingCircle /> : children}
     </S.Button>
   );
 };
