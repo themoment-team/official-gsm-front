60eec61e3609d3d2fd768c6aacbb794540ee813e
yoosion030 <yoosioff@gmail.com>
2023-05-18 15:39:48 +0900

    Update px -> rem

--- apps/admin/src/components/Header/style.ts
+++ apps/admin/src/components/Header/style.ts
@@ -7,17 +7,15 @@ export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  height: 80px;
+  height: 5rem;
   width: 100vw;
   background-color: rgba(255, 255, 255, 0.9);
-  backdrop-filter: blur(11px);
+  backdrop-filter: blur(0.625rem);
   left: 0px;
   top: 0px;
   padding: 0 4.8vw;
-
   font-weight: 600;
-  font-size: 14px;
-  line-height: 20px;
+  font-size: 0.875rem;
 
   a {
     text-decoration: none;
@@ -45,9 +43,9 @@ export const Notofication = styled.div`
 export const Notification = styled.div`
   position: absolute;
   top: 0;
-  right: -5px;
-  width: 4px;
-  height: 4px;
+  right: -0.313rem;
+  width: 0.25rem;
+  height: 0.25rem;
   background-color: #f93535;
   border-radius: 50%;
 `;
