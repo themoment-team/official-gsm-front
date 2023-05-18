faadf99437e64317b2ed110b056681e724905e62
yoosion030 <yoosioff@gmail.com>
2023-05-18 19:57:32 +0900

    Add ApproveModal Component

---
+++ apps/admin/src/components/ApproveModal/index.tsx
@@ -0,0 +1,15 @@
+import * as S from "./style";
+// import * as I from "assets";
+import * as I from "../../assets";
+
+const ApproveModal = () => {
+  return (
+    <S.ModalConatiner>
+      <S.ModalHeader>
+        🤝 가입 요청 <I.ModalCloseIcon />
+      </S.ModalHeader>
+    </S.ModalConatiner>
+  );
+};
+
+export default ApproveModal;
---
+++ apps/admin/src/components/ApproveModal/style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const ModalConatiner = styled.div`
+  position: absolute;
+  width: 331px;
+  height: 270px;
+  left: 237px;
+  top: 104px;
+  background: #fdfdfd;
+  box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.11);
+  border-radius: 20px;
+  padding: 26px 22px 42px 16px;
+`;
+
+export const ModalHeader = styled.div``;
