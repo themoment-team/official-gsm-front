d54190109023c13ed4c0671e6c99feb770206127
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-18 22:11:31 +0900

    Add AuthDescription

---
+++ apps/admin/src/components/AuthDescription/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Description } from "./style";
+
+interface AuthDescriptionProps {
+  children: React.ReactNode;
+}
+
+/**
+ * `auth/*` path 에서 사용되는 타이틀
+ *
+ * @prop children: React.ReactNode
+ */
+const AuthDescription: React.FC<AuthDescriptionProps> = ({ children }) => {
+  return <Description>{children}</Description>;
+};
+
+export default AuthDescription;
