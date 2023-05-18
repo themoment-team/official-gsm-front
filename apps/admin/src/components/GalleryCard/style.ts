8daefe21a7dfd7fd5faf7e927020a951be55eb75
Lee-Seungje <moondgod@gmail.com>
2023-05-18 20:57:44 +0900

    Delete font-feature-settings

--- apps/admin/src/components/GalleryCard/style.ts
+++ apps/admin/src/components/GalleryCard/style.ts
@@ -25,7 +25,6 @@ export const Title = styled.h2`
   font-weight: 700;
   font-size: 1.125rem;
   line-height: 1.5625rem;
-  font-feature-settings: "ss10" on;
   color: #000000;
   margin-bottom: 0.5rem;
 `;
@@ -35,7 +34,6 @@ export const Text = styled.pre`
   font-size: 0.9375rem;
   line-height: 1.5rem;
   letter-spacing: 0.0096em;
-  font-feature-settings: "ss10" on;
   color: #505050;
   margin-bottom: 1.5rem;
 `;
@@ -50,6 +48,5 @@ export const Details = styled.p`
   font-weight: 600;
   font-size: 0.8125rem;
   line-height: 1rem;
-  font-feature-settings: "ss10" on;
   color: #828282;
 `;
