12e59b2e6ae1929fad21fdf6fe5e140bf8b99d8a
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-18 22:33:05 +0900

    Update AuthModel

--- apps/admin/src/components/AuthModel/index.tsx
+++ apps/admin/src/components/AuthModel/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Video } from "./style";
+import { css } from "@emotion/react";
 
 interface AuthModelProps {
   modelUrl: string;
+  marginTopREM?: number;
 }
 
-const AuthModel: React.FC<AuthModelProps> = ({ modelUrl }) => {
-  return <Video src={modelUrl} autoPlay={true} muted={true} loop />;
+const AuthModel: React.FC<AuthModelProps> = ({ modelUrl, marginTopREM }) => {
+  return (
+    <Video
+      src={modelUrl}
+      autoPlay={true}
+      muted={true}
+      loop
+      css={css`
+        margin-top: ${marginTopREM && `${marginTopREM}rem`};
+      `}
+    />
+  );
 };
 
 export default AuthModel;
