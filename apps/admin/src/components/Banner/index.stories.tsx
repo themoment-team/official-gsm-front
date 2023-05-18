9e1639608333715c37437d6b2ef1860b3824fb7a
Lee-Seungje <moondgod@gmail.com>
2023-05-19 07:47:06 +0900

    Add Banner stories

---
+++ apps/admin/src/components/Banner/index.stories.tsx
@@ -0,0 +1,16 @@
+import Banner from ".";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "admin/Banner",
+  component: Banner,
+  parameters: {},
+} as Meta<typeof Banner>;
+
+type Story = StoryObj<typeof Banner>;
+
+export const Primary: Story = {
+  args: {
+    modelUrl: "/models/school.webm",
+  },
+};
