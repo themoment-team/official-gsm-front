9278021a65a6f2afff2e59d89c1f74ae88f51993
yoosion030 <yoosioff@gmail.com>
2023-05-18 15:35:46 +0900

    Add Header Style

---
+++ apps/admin/src/components/Header/index.stories.tsx
@@ -0,0 +1,25 @@
+import Header from ".";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "admin/Header",
+  component: Header,
+  parameters: {},
+} as Meta<typeof Header>;
+
+type Story = StoryObj<typeof Header>;
+
+export const Primary: Story = {
+  args: {
+    hasNotification: false,
+    name: "정문정",
+  },
+};
+
+export const HasNotification: Story = {
+  args: {
+    ...Primary.args,
+    hasNotification: true,
+    name: "정문정",
+  },
+};
