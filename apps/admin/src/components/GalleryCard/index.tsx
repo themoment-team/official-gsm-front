940221673071911f05c68d16f67f487c8530b7e5
Lee-Seungje <moondgod@gmail.com>
2023-05-18 20:23:39 +0900

    Update subTitle -> description

--- apps/admin/src/components/GalleryCard/index.tsx
+++ apps/admin/src/components/GalleryCard/index.tsx
@@ -3,7 +3,7 @@ import * as S from "./style";
 interface GalleryCardProps {
   imgUrl: string;
   title: string;
-  subTitle: string;
+  description: string;
   writer: string;
   date: string;
 }
@@ -11,7 +11,7 @@ interface GalleryCardProps {
 const GalleryCard: React.FC<GalleryCardProps> = ({
   imgUrl,
   title,
-  subTitle,
+  description,
   writer,
   date,
 }) => {
@@ -21,7 +21,7 @@ const GalleryCard: React.FC<GalleryCardProps> = ({
         <S.ContentIMG width={349} height={220} src={imgUrl} alt="ContentIMG" />
       </S.IMGWrpapper>
       <S.Title>{title}</S.Title>
-      <S.Text>{subTitle}</S.Text>
+      <S.Text>{description}</S.Text>
       <S.DetailWrpapper>
         <S.Details>{writer}</S.Details>
         <S.Details>{date}</S.Details>
