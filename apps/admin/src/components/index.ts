95a2fdfb1212719239d76764b78e3d683fd86271
이정우 <comand123@naver.com>
2023-05-20 02:04:41 +0900

    add InputBox export

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ToBackButton } from "./ToBackButton";
 export { default as AuthTitle } from "./AuthTitle";
 export { default as AuthModel } from "./AuthModel";
 export { default as Header } from "./Header";
+export { default as InputBox } from "./InputBox";
