d85bc3503191efb8143d7b81e74ed0dbe29d845f
Lee-Seungje <moondgod@gmail.com>
2023-05-21 14:53:10 +0900

    Add exports

--- apps/admin/src/assets/index.ts
+++ apps/admin/src/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as ToBackIcon } from "./svg/icon/ToBackIcon";
 export { default as HeaderRectangle } from "./svg/icon/HeaderRectangle";
+export { default as WriteIcon } from "./svg/icon/WriteIcon";
+export { default as FileIcon } from "./svg/icon/FileIcon";
