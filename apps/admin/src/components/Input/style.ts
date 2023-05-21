ef9a2525a35c4e1ddc10660e01ed369a67161971
이정우 <comand123@naver.com>
2023-05-22 08:40:00 +0900

    edit component padding

--- apps/admin/src/components/Input/style.ts
+++ apps/admin/src/components/Input/style.ts
@@ -5,7 +5,7 @@ export const InputBox = styled.input`
   border: 0.0625rem solid #dcdcdc;
   box-sizing: border-box;
   background-color: #fff;
-  padding-left: 20px;
+  padding: 0 20px 0 20px;
   font-size: 0.75rem;
   color: #a4a4a4;
   transition: ease-in-out 0.3s;
