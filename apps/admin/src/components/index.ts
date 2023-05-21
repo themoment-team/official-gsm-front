69dc070ea0fc5ae82d44f15d323e6b221f90611b
이정우 <comand123@naver.com>
2023-05-21 23:19:53 +0900

    add default

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as AuthTitle } from "./AuthTitle";
 export { default as AuthModel } from "./AuthModel";
 export { default as AuthDescription } from "./AuthDescription";
 export { default as Header } from "./Header";
+export { default as TextArea } from "./TextArea";
