705a664afc220982d48209c7f86da8ece678dc20
Lee-Seungje <moondgod@gmail.com>
2023-05-21 16:45:16 +0900

    Update export

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -4,3 +4,4 @@ export { default as AuthModel } from "./AuthModel";
 export { default as AuthDescription } from "./AuthDescription";
 export { default as Header } from "./Header";
 export { default as DetailPageHeader } from "./DetailPageHeader";
+export { default as PostContent } from "./PostContent";
