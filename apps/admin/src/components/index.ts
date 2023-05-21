11ec3c33a33aae392a1a719e9c1f743f49245825
이정우 <comand123@naver.com>
2023-05-21 14:36:54 +0900

    add Input,TextAre default

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -2,4 +2,5 @@ export { default as ToBackButton } from "./ToBackButton";
 export { default as AuthTitle } from "./AuthTitle";
 export { default as AuthModel } from "./AuthModel";
 export { default as Header } from "./Header";
-export { default as InputBox } from "./InputBox";
+export { default as Input } from "./Input";
+export { default as TextArea } from "./TextArea";
