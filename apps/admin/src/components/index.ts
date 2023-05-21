7b501a834eb21df62a66e6eee827422385ecbbaf
이정우 <comand123@naver.com>
2023-05-21 23:04:34 +0900

    delet textare component

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -3,4 +3,3 @@ export { default as AuthTitle } from "./AuthTitle";
 export { default as AuthModel } from "./AuthModel";
 export { default as Header } from "./Header";
 export { default as Input } from "./Input";
-export { default as TextArea } from "./TextArea";
