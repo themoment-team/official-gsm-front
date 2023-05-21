34f03b51e8d0d130f0831d5b5421d14c0ebc2926
Lee-Seungje <moondgod@gmail.com>
2023-05-21 12:40:04 +0900

    Update Banner prop way

--- apps/admin/src/components/Banner/index.tsx
+++ apps/admin/src/components/Banner/index.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { BannerWrapper, Video } from "./style";
 
 interface BannerProps {
   modelUrl: string;
 }
 
-const Banner = ({ modelUrl }: BannerProps) => {
+const Banner: React.FC<BannerProps> = ({ modelUrl }) => {
   return (
     <BannerWrapper>
       <Video src={modelUrl} autoPlay={true} muted={true} loop />
