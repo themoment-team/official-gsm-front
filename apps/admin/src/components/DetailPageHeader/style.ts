61caa40ab92cd97085e8b7b4496ca7f4648e314e
Lee-Seungje <moondgod@gmail.com>
2023-05-21 15:39:41 +0900

    Update px -> rem

--- apps/admin/src/components/DetailPageHeader/style.ts
+++ apps/admin/src/components/DetailPageHeader/style.ts
@@ -2,26 +2,26 @@ import styled from "@emotion/styled";
 
 export const Header = styled.header`
   width: 100vw;
-  height: 80px;
-  padding: 0 241px 0 246px;
+  height: 5rem;
+  padding: 0 15.0625rem 0 15.375rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
 
 export const UpdateBox = styled.div`
-  width: 124px;
+  width: 7.75rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
 export const AddFileBTN = styled.div`
-  width: 89px;
-  height: 27px;
-  padding: 5px 10px 6px 12.67px;
+  width: 5.5625rem;
+  height: 1.6875rem;
+  padding: 0.3125rem 0.625rem 0.375rem 0.7919rem;
   background: #eeeeee;
-  border-radius: 20px;
+  border-radius: 1.25rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -29,7 +29,7 @@ export const AddFileBTN = styled.div`
 
 export const AddFileText = styled.p`
   font-weight: 400;
-  font-size: 13px;
-  line-height: 16px;
+  font-size: 0.8125rem;
+  line-height: 1rem;
   color: #828282;
 `;
