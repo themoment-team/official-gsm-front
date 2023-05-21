8101b0738d2eaa72a75b9217f21fb5692cf65d4c
Lee-Seungje <moondgod@gmail.com>
2023-05-21 16:46:53 +0900

    Update IconSize

--- apps/admin/src/assets/svg/icon/FileIcon.tsx
+++ apps/admin/src/assets/svg/icon/FileIcon.tsx
@@ -1,7 +1,7 @@
 const FileIcon = () => (
   <svg
-    width="16"
-    height="20"
+    width="10.67"
+    height="13.33"
     viewBox="0 0 16 20"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
