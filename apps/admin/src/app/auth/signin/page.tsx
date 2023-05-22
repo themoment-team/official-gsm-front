cf47fc000ec6ce7177af2ef4ca72f6129f70367f
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-22 15:19:09 +0900

    Add SigninPage

--- apps/admin/src/app/auth/signin/page.tsx
+++ apps/admin/src/app/auth/signin/page.tsx
@@ -1,35 +1,61 @@
 "use client";
 
 import styled from "@emotion/styled";
-import { AuthTitle } from "components";
+import { AuthError, AuthTitle, Input } from "components";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "ui";
 
 export default function SigninPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const { back } = useRouter();
+  const [isSigninLoading, setIsSigninLoading] = useState<boolean>(false);
 
   return (
     <>
       <AuthTitle marginTopREM={1.5} textAlign="center" fontSizeREM={1.75}>
         교사 로그인
       </AuthTitle>
-      <Button
-        position="absolute"
-        bottomREM={1}
-        onClick={() => {
-          setIsLoading(true);
-        }}
-        isLoading={isLoading}
-      >
-        <CustomLink href="/auth/signup">작성</CustomLink>
-      </Button>
+      <Form>
+        <InputBox>
+          <Input placeholder="아이디를 입력해주세요." type="text" />
+          <Input type="password" placeholder="비밀번호를 입력해주세요." />
+        </InputBox>
+        <AuthError>아이디 혹은 비밀번호를 다시 확인해주세요.</AuthError>
+        <ButtonBox>
+          <Button
+            onClick={() => {
+              setIsSigninLoading(true);
+            }}
+            isLoading={isSigninLoading}
+            type="submit"
+          >
+            로그인
+          </Button>
+          <Button bgColor="#DEE449" hoverBgColor="#E9EC9E" fontColor="#050505">
+            <CustomLink href="/auth/signup">회원가입</CustomLink>
+          </Button>
+        </ButtonBox>
+      </Form>
     </>
   );
 }
+
+const Form = styled.form`
+  margin-top: 3rem;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const ButtonBox = styled.div`
+  position: absolute;
+  bottom: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 const CustomLink = styled(Link)`
   width: 100%;
