f77c74cad5f3de606d704077b0e91304b5f9d198
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-22 13:57:07 +0900

    Add SigninPage

--- apps/admin/src/app/auth/signin/page.tsx
+++ apps/admin/src/app/auth/signin/page.tsx
@@ -1,5 +1,40 @@
 "use client";
 
+import styled from "@emotion/styled";
+import { AuthTitle } from "components";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "ui";
+
 export default function SigninPage() {
-  return <div>signin</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const { back } = useRouter();
+
+  return (
+    <>
+      <AuthTitle marginTopREM={1.5} textAlign="center" fontSizeREM={1.75}>
+        교사 로그인
+      </AuthTitle>
+      <Button
+        position="absolute"
+        bottomREM={1}
+        onClick={() => {
+          setIsLoading(true);
+        }}
+        isLoading={isLoading}
+      >
+        <CustomLink href="/auth/signup">작성</CustomLink>
+      </Button>
+    </>
+  );
 }
+
+const CustomLink = styled(Link)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
