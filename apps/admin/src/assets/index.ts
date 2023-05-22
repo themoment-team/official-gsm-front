ae97f7cc3e9b69623b6928a36cd1337e19490a6d
Lee-Seungje <moondgod@gmail.com>
2023-05-23 00:39:20 +0900

    Delete HeaderRectangle import

--- apps/admin/src/assets/index.ts
+++ apps/admin/src/assets/index.ts
@@ -1,5 +1,4 @@
 export { default as ToBackIcon } from "./svg/icon/ToBackIcon";
-export { default as HeaderRectangle } from "./svg/icon/HeaderRectangle";
 export { default as WriteIcon } from "./svg/icon/WriteIcon";
 export { default as FileIcon } from "./svg/icon/FileIcon";
 export { default as VerticalBarIcon } from "./svg/icon/VerticalBarIcon";
