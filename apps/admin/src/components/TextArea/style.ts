c3de2d4a5328b0497c42ea009d4a7c18c4dd029f
이정우 <comand123@naver.com>
2023-05-22 10:21:08 +0900

    Update border style

--- apps/admin/src/components/TextArea/style.ts
+++ apps/admin/src/components/TextArea/style.ts
@@ -3,17 +3,19 @@ import styled from "@emotion/styled";
 export const TextAreaBox = styled.textarea`
   border-radius: 1.25rem;
   border: 0.0625rem solid #dcdcdc;
-  box-sizing: border-box;
+  outline: none;
   background-color: #fff;
   padding: 0.875rem 0 0 1.25rem;
   font-size: 0.75rem;
   color: #a4a4a4;
   resize: none;
   transition: ease-in-out 0.3s;
+
   :hover {
     border: 0.0625rem solid #050505;
   }
   :focus {
+    border: 0.0625rem solid #050505;
     color: #505050;
   }
   ::placeholder {
