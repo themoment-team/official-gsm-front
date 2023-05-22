30818344b2abcf54f383bbc06c80b70d6c637320
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-23 08:32:28 +0900

    Add SignupPage

--- apps/admin/src/app/auth/signup/page.tsx
+++ apps/admin/src/app/auth/signup/page.tsx
@@ -1,5 +1,24 @@
 "use client";
 
-export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+import { AuthTitle, ToBackButton, Input } from "components";
+import { useRouter } from "next/navigation";
+import { Button } from "ui";
+
+export default function SignupPage() {
+  const { replace } = useRouter();
+
+  return (
+    <>
+      <ToBackButton onClick={() => replace("/auth/signin")} />
+      <AuthTitle>
+        저희 학교 선생님인지
+        <br />
+        확인하기 위해 성함을 입력해주세요.
+      </AuthTitle>
+      <Input placeholder="성함을 입력해주세요." />
+      <Button position="absolute" bottom="1.5rem">
+        다음
+      </Button>
+    </>
+  );
 }
