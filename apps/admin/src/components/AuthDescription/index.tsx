b458c55064a1c2fc8c9c012279d0306bff435dd7
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-23 16:15:19 +0900

    Add className prop in AuthDescription for styled override

--- apps/admin/src/components/AuthDescription/index.tsx
+++ apps/admin/src/components/AuthDescription/index.tsx
@@ -3,6 +3,7 @@ import { Description } from "./style";
 
 interface AuthDescriptionProps {
   children: React.ReactNode;
+  className?: string;
 }
 
 /**
@@ -10,8 +11,11 @@ interface AuthDescriptionProps {
  *
  * @prop children: React.ReactNode
  */
-const AuthDescription: React.FC<AuthDescriptionProps> = ({ children }) => {
-  return <Description>{children}</Description>;
+const AuthDescription: React.FC<AuthDescriptionProps> = ({
+  children,
+  className,
+}) => {
+  return <Description className={className}>{children}</Description>;
 };
 
 export default AuthDescription;
