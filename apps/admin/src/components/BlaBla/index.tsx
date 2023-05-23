b0ce2c566e486cfdb17f9eb6f1afee46d1581808
Lee-Seungje <moondgod@gmail.com>
2023-05-23 15:00:53 +0900

    Add Wrapper

---
+++ apps/admin/src/components/BlaBla/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./style";
+
+const BlaBla = () => {
+  return (
+    <S.BlaWrapper>
+      <S.BlaHeader></S.BlaHeader>
+    </S.BlaWrapper>
+  );
+};
+
+export default BlaBla;
---
+++ apps/admin/src/components/BlaBla/style.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const BlaWrapper = styled.div`
+  width: 1080px;
+  height: 383px;
+`;
+
+export const BlaHeader = styled.header`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
