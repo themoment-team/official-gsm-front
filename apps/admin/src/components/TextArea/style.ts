f2d86d4d8f4fb4d7556278493bdee2a917f46366
이정우 <comand123@naver.com>
2023-05-23 14:04:33 +0900

    Update style code refactoring

--- apps/admin/src/components/TextArea/style.ts
+++ apps/admin/src/components/TextArea/style.ts
@@ -8,20 +8,22 @@ export const TextAreaBox = styled.textarea`
   padding: 0.875rem 1.25rem 0.875rem 1.25rem;
   font-size: 0.75rem;
   color: #a4a4a4;
-
   resize: none;
   transition: ease-in-out 0.3s;
 
   ::-webkit-scrollbar {
     display: none;
   }
+
   :hover {
     border: 0.0625rem solid #050505;
   }
+
   :focus {
     border: 0.0625rem solid #050505;
     color: #505050;
   }
+
   ::placeholder {
     color: #a4a4a4;
   }
