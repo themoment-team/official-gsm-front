7d49ff81eb6074b9aab03d6f1c27719ec5a02880
이정우 <comand123@naver.com>
2023-05-23 14:00:56 +0900

    Update scrollbar style

--- apps/admin/src/components/TextArea/style.ts
+++ apps/admin/src/components/TextArea/style.ts
@@ -8,9 +8,13 @@ export const TextAreaBox = styled.textarea`
   padding: 0.875rem 1.25rem 0.875rem 1.25rem;
   font-size: 0.75rem;
   color: #a4a4a4;
+
   resize: none;
   transition: ease-in-out 0.3s;
 
+  ::-webkit-scrollbar {
+    display: none;
+  }
   :hover {
     border: 0.0625rem solid #050505;
   }
