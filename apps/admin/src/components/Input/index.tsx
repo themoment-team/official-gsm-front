d1f8ef651ee25d969ce6627525d5dac277faab57
이정우 <comand123@naver.com>
2023-05-24 19:24:25 +0900

    Update InputPage

--- apps/admin/src/components/Input/index.tsx
+++ apps/admin/src/components/Input/index.tsx
@@ -1,6 +1,14 @@
+"use client";
+
 import { css } from "@emotion/react";
 import * as S from "./style";
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useState,
+  useRef,
+} from "react";
+import { InputValueResetBtn } from "../../assets";
 
 interface InputProps
   extends DetailedHTMLProps<
@@ -9,17 +17,63 @@ interface InputProps
   > {
   width?: string;
   height?: string;
+  resetBtn?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({ width, height, ...attributes }) => {
+const Input: React.FC<InputProps> = ({
+  width,
+  height,
+  resetBtn = false,
+  ...attributes
+}) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleDivClick = () => {
+    if (inputRef.current) {
+      setIsInputFocused(true);
+      setInputValue("");
+      inputRef.current.focus();
+    }
+  };
+
   return (
-    <S.InputBox
-      css={css`
-        width: ${width};
-        height: ${height};
-      `}
-      {...attributes}
-    />
+    <S.InputWrapper isResetBtn={resetBtn}>
+      {resetBtn ? (
+        <div
+          onClick={() => handleDivClick()}
+          css={css`
+            position: absolute;
+            right: 1rem;
+            cursor: pointer;
+            opacity: 0;
+            transition: ease-in-out 0.3s;
+            ${isInputFocused
+              ? css`
+                  opacity: 1;
+                `
+              : ""}
+          `}
+        >
+          <InputValueResetBtn />
+        </div>
+      ) : (
+        ""
+      )}
+      <S.InputBox
+        css={css`
+          width: ${width};
+          height: ${height};
+        `}
+        {...attributes}
+        ref={inputRef}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onFocus={() => setIsInputFocused(true)}
+        onBlur={() => setIsInputFocused(false)}
+      />
+    </S.InputWrapper>
   );
 };
 
