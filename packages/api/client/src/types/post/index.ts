650d752e1bc25373b52548293300e54ac3c4363b
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-25 17:09:10 +0900

    Add Post type

---
+++ packages/api/client/src/types/post/index.ts
@@ -0,0 +1,28 @@
+export type PostCategoryType = "NOTICE" | "FAMILY_NEWSLETTER" | "EVENT_GALLERY";
+
+export interface PostDetailType {
+  postTitle: string;
+  postWriter: string;
+  postContent: string;
+  createdAt: string;
+  fileInfo: FileInfoType[];
+}
+
+interface FileInfoType {
+  fileUrl: string;
+  fileName: string;
+  fileExtension: string;
+}
+
+export interface PostListType {
+  content: [
+    {
+      postSeq: number;
+      postTitle: string;
+      postWriter: string;
+      createdAt: string;
+      thumbnailUrl: string;
+      fileIsExist: boolean;
+    }
+  ];
+}
