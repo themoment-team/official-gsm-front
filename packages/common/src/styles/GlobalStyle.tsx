a74fdd32bd9e1cf4897afcd6f35b4b02e3e442a6
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-25 14:35:55 +0900

    Update common

--- packages/common/src/styles/GlobalStyle.tsx
+++ packages/common/src/styles/GlobalStyle.tsx
@@ -1,7 +1,7 @@
 import { Global, css } from "@emotion/react";
 import emotionReset from "emotion-reset";
 
-export default function GlobalStyle() {
+export const GlobalStyle = () => {
   return (
     <Global
       styles={css`
@@ -29,4 +29,4 @@ export default function GlobalStyle() {
       `}
     />
   );
-}
+};
