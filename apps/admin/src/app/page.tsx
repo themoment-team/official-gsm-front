e49992593d7270a8a0e62d72de153de08c0bf2d0
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-29 16:44:12 +0900

    Add example codes

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { Header } from "components";
+import { useGetPostDetail, useGetPostList } from "api/client";
 
 export default function Home() {
+  // example
+  const { data: postList, isLoading } = useGetPostList(0, "NOTICE");
+
+  const { data: postDetail } = useGetPostDetail(1);
+
+  console.log(postList);
+  console.log(postDetail);
+
   return (
     <div>
       <Header hasNotification={false} name={"정문정"} />
