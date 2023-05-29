b84486128f62869fb0330049f80b5046ad65a266
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-29 16:41:07 +0900

    Add useGetPostDetail

---
+++ packages/api/client/src/hooks/api/post/useGetPostDetail.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { postUrl, get, PostDetailType, postQueryKeys } from "client";
+
+export const useGetPostDetail = (seq: number) => {
+  const query = useQuery<PostDetailType>(postQueryKeys.getPostDetail(seq), () =>
+    get(postUrl.postDetail(seq))
+  );
+
+  return query;
+};
