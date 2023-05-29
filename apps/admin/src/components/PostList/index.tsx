89a5c16bacd8acd57c9aaad43f94430d1c2c263f
Lee-Seungje <moondgod@gmail.com>
2023-05-30 00:52:41 +0900

    Add postList map

--- apps/admin/src/components/PostList/index.tsx
+++ apps/admin/src/components/PostList/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PostListHeader from "../PostListHeader";
+import { useState } from "react";
 
 interface PostListHeader {
   kind: string;
@@ -7,10 +8,26 @@ interface PostListHeader {
   isAdmin: boolean;
 }
 
+interface PostType {
+  postSeq: number;
+  postTitle: string;
+  postWriter: string;
+  createdAt: Date;
+  thumbnailUrl: string;
+  fileIsExist: boolean;
+}
+
 const PostList: React.FC<PostListHeader> = ({ kind, seeMore, isAdmin }) => {
+  const [postList, setPostList] = useState<PostType[]>();
+
   return (
     <>
       <PostListHeader kind={kind} seeMore={seeMore} isAdmin={isAdmin} />
+      {
+        postList?.map(() => {
+          s
+        })
+      }
     </>
   );
 };
