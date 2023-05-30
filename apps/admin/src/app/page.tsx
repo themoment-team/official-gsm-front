8f49283a64cda94816511eef3a01e85dd0c5c687
이정우 <comand123@naver.com>
2023-05-30 13:33:59 +0900

    Update Test

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { Header } from "components";
+import { Header, Input } from "components";
+import { useRef } from "react";
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div>
       <Header hasNotification={false} name={"정문정"} />
+      <Input
+        ref={inputRef}
+        onChange={() => console.log(inputRef?.current?.value)}
+      />
     </div>
   );
 }
