bdcd83a97cdc6cf3a246d56eef2311fb44b77fbe
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-30 15:19:34 +0900

    Add useForwardRef hook in Input component

--- apps/admin/src/components/Input/index.tsx
+++ apps/admin/src/components/Input/index.tsx
@@ -2,8 +2,9 @@
 
 import { css } from "@emotion/react";
 import * as S from "./style";
-import { DetailedHTMLProps, InputHTMLAttributes, useRef } from "react";
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef } from "react";
 import { InputValueResetBtnIcon } from "../../assets";
+import { useForwardRef } from "../../hooks";
 
 interface InputProps
   extends DetailedHTMLProps<
@@ -15,37 +16,36 @@ interface InputProps
   resetBtn?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({
-  width,
-  height,
-  resetBtn = false,
-  ref = null,
-  ...attributes
-}) => {
-  const handleDivClick = () => {
-    if (ref) {
-      console.log(ref.current.value);
-      inputRef.current.value = "";
-      inputRef.current.focus();
-    }
-  };
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ width, height, resetBtn = false, ...attributes }, ref) => {
+    const inputRef = useForwardRef<HTMLInputElement>(ref);
 
-  return (
-    <S.InputWrapper
-      isResetBtn={resetBtn}
-      css={css`
-        width: ${width};
-        height: ${height};
-      `}
-    >
-      <S.InputBox {...attributes} ref={ref} />
-      {resetBtn && (
-        <S.InputValueResetBtn onClick={() => handleDivClick()}>
-          <InputValueResetBtnIcon />
-        </S.InputValueResetBtn>
-      )}
-    </S.InputWrapper>
-  );
-};
+    const handleDivClick = () => {
+      if (inputRef) {
+        inputRef.current.value = "";
+        inputRef.current.focus();
+      }
+    };
+
+    return (
+      <S.InputWrapper
+        isResetBtn={resetBtn}
+        css={css`
+          width: ${width};
+          height: ${height};
+        `}
+      >
+        <S.InputBox {...attributes} ref={inputRef} />
+        {resetBtn && (
+          <S.InputValueResetBtn onClick={() => handleDivClick()}>
+            <InputValueResetBtnIcon />
+          </S.InputValueResetBtn>
+        )}
+      </S.InputWrapper>
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
