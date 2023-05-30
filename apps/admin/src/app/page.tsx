0058fa6ab7e51c35d735d91bc8884ea1dd2cbc7f
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-30 15:15:53 +0900

    Remove test Input

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { Header, Input } from "components";
-import { useRef } from "react";
+import { Header } from "components";
 
 export default function Home() {
-  const inputRef = useRef<HTMLInputElement>(null);
-
   return (
     <div>
       <Header hasNotification={false} name={"정문정"} />
-      <Input
-        ref={inputRef}
-        onChange={() => console.log(inputRef?.current?.value)}
-      />
     </div>
   );
 }
