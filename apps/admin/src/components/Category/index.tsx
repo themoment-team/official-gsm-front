caf9bbe28e84fde5570bb33e5b3b720e376e1c30
hyeongrok7874 <khr5k0228@gmail.com>
2023-05-31 12:21:09 +0900

    Update Category

--- apps/admin/src/components/Category/index.tsx
+++ apps/admin/src/components/Category/index.tsx
@@ -1,33 +1,32 @@
+import { CategoryType } from "admin/types";
 import * as S from "./style";
 import Link from "next/link";
 
 interface CategoryProps {
-  category?: string;
+  category: CategoryType;
 }
 
-const test = [
-  { path: "/notice", label: "공지사항" },
+const categoryArray = [
+  { path: "/", label: "공지사항" },
+  { path: "/newsletter", label: "가정통신문" },
   { path: "/gallery", label: "행사갤러리" },
-  { path: "/familyCorrespondence", label: "가정통신문" },
 ];
 
 const Category: React.FC<CategoryProps> = ({ category }) => {
   return (
     <S.Category>
       <S.MenuWrap>
-        {test.map((tab) => {
-          return (
-            <S.Menu key={tab.path}>
-              <Link href={tab.path}>
-                <S.LinkWrap>
-                  <S.Path isActive={category === tab.path}>
-                    ∙&nbsp;&nbsp;{tab.label}
-                  </S.Path>
-                </S.LinkWrap>
-              </Link>
-            </S.Menu>
-          );
-        })}
+        {categoryArray.map(({ path, label }) => (
+          <S.Menu key={path}>
+            <Link href={path}>
+              <S.LinkWrap>
+                <S.Path isActive={`/${category}` === path}>
+                  ∙&nbsp;&nbsp;{label}
+                </S.Path>
+              </S.LinkWrap>
+            </Link>
+          </S.Menu>
+        ))}
       </S.MenuWrap>
     </S.Category>
   );
