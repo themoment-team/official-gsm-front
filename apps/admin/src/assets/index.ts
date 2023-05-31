45df3f424a4677979cf52ab10bed9f2f1b990e2d
이정우 <comand123@naver.com>
2023-06-01 02:52:41 +0900

    Add Plus Icon

--- apps/admin/src/assets/index.ts
+++ apps/admin/src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as ToBackIcon } from "./svg/icon/ToBackIcon";
 export { default as VerticalBarIcon } from "./svg/icon/VerticalBarIcon";
 export { default as ModalCloseIcon } from "./svg/icon/ModalCloseIcon";
 export { default as InputValueResetBtnIcon } from "./svg/icon/InputValueResetBtnIcon";
+export { default as PlusIcon } from "./svg/icon/PlusIcon";
