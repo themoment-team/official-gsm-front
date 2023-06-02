2a029b80768152ad7aaeff0e2072cc8dcf3f100b
이정우 <comand123@naver.com>
2023-06-02 15:38:11 +0900

    Update path

--- apps/admin/src/app/post/write/page.tsx
+++ apps/admin/src/app/post/write/page.tsx
@@ -1,34 +1,33 @@
-"use client";
+'use client';
 
-import { css } from "@emotion/react";
-import { Input, TextArea, Category } from "components";
-import { PlusIcon } from "assets";
-import * as S from "./style";
+import { Input, TextArea, Category } from 'admin/components';
+import { PlusIcon } from 'admin/assets';
+import * as S from './style';
 
 export default function WritePage() {
-  const UploadTitleArr = ["배너 사진", "첨부 파일"];
+  const UploadTitleArr = ['배너 사진', '첨부 파일'];
   return (
     <S.WritePageWrap>
       <S.WriteTitle>게시물 생성</S.WriteTitle>
       <S.FormWrap>
         <div>
           <S.FormItemTitle>카테고리</S.FormItemTitle>
-          <Category width="36.125rem" />
+          <Category width='36.125rem' />
         </div>
         <div>
           <S.FormItemTitle>제목</S.FormItemTitle>
           <Input
-            placeholder="제목을 입력해주세요."
-            width="36.125rem"
-            height="2.75rem"
+            placeholder='제목을 입력해주세요.'
+            width='36.125rem'
+            height='2.75rem'
           />
         </div>
         <div>
           <S.FormItemTitle>내용</S.FormItemTitle>
           <TextArea
-            placeholder="내용을 입력해주세요."
-            width="36.125rem"
-            height="6.75rem"
+            placeholder='내용을 입력해주세요.'
+            width='36.125rem'
+            height='6.75rem'
           />
         </div>
         {UploadTitleArr.map((title, id) => {
