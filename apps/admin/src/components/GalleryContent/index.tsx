1c91b59e1526c089f08330265381db58834880ee
Lee-Seungje <moondgod@gmail.com>
2023-06-03 14:40:50 +0900

    Update galleryContent props

--- apps/admin/src/components/GalleryContent/index.tsx
+++ apps/admin/src/components/GalleryContent/index.tsx
@@ -1,15 +1,35 @@
 import * as S from './style';
 import Image from 'next/image';
 
-const GalleryContent = ({}) => {
+interface FileInfo {
+  fileUrl: string;
+  fileName: string;
+}
+
+interface PostType {
+  postSeq: number;
+  postTitle: string;
+  postWriter: string;
+  createdAt: string;
+  thumbnailUrl: string | null;
+  fileIsExist: boolean;
+}
+
+interface GalleryContentProps {
+  fileInfo: FileInfo[];
+  description: string;
+  post: PostType;
+}
+
+const GalleryContent: React.FC<GalleryContentProps> = ({
+  post: { thumbnailUrl, postWriter, postTitle, createdAt },
+  fileInfo,
+  description,
+}) => {
   return (
     <>
       <S.IMGWrapper>
-        <Image
-          alt='content image'
-          src='https://i.ytimg.com/vi/FBEBPnWafTk/mqdefault.jpg'
-          fill
-        />
+        <Image alt='content image' src={thumbnailUrl ?? ''} fill />
       </S.IMGWrapper>
     </>
   );
