4ff6f95798fe81d8ca282b2a3b92b3b84ad9ada3
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-06 05:05:19 +0900

    Update stories

--- apps/admin/src/components/PostList/index.stories.tsx
+++ apps/admin/src/components/PostList/index.stories.tsx
@@ -1,10 +1,9 @@
-import PostList from ".";
+import PostList from '.';
 
-import type { Meta, StoryObj } from "@storybook/react";
-
+import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
-  title: "admin/PostList",
+  title: 'admin/PostList',
   component: PostList,
   parameters: {},
 } as Meta<typeof PostList>;
@@ -14,35 +13,32 @@ type Story = StoryObj<typeof PostList>;
 const postList = [
   {
     postSeq: 1,
-    postTitle: "Test",
-    postWriter: "Tester",
-    createdAt: "2023-05-03T19:47:01.250197",
-    thumbnailUrl: null,
-    fileIsExist: true,
+    postTitle: 'Title 1',
+    postWriter: '선생님',
+    createdAt: '2023-05-03T19:47:01.250197',
+    contentPreview: '1.코로나-19에 따른 학습 공백 최소화를',
   },
   {
-    postSeq: 1,
-    postTitle: "Test",
-    postWriter: "Tester",
-    createdAt: "2023-05-03T19:47:01.250197",
-    thumbnailUrl: null,
-    fileIsExist: true,
+    postSeq: 2,
+    postTitle: 'Title 2',
+    postWriter: '선생님',
+    createdAt: '2023-05-03T19:47:01.250197',
+    contentPreview: '',
   },
   {
-    postSeq: 1,
-    postTitle: "Test",
-    postWriter: "Tester",
-    createdAt: "2023-05-03T19:47:01.250197",
-    thumbnailUrl: null,
-    fileIsExist: true,
+    postSeq: 3,
+    postTitle: 'Title 3',
+    postWriter: '선생님',
+    createdAt: '2023-05-03T19:47:01.250197',
+    contentPreview: '1.코로나-19에 따른 학습 공백 최소화를 위하여 한국교교',
   },
   {
-    postSeq: 1,
-    postTitle: "Test",
-    postWriter: "Tester",
-    createdAt: "2023-05-03T19:47:01.250197",
-    thumbnailUrl: null,
-    fileIsExist: true,
+    postSeq: 4,
+    postTitle: 'Title 4',
+    postWriter: '선생님',
+    createdAt: '2023-05-03T19:47:01.250197',
+    contentPreview:
+      '1.코로나-19에 따른 학습 공백 최소화를 위하여 한국교교 안녕하세요 안녕하세요 안녕하세요',
   },
 ];
 
