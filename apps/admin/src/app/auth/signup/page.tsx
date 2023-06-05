64457bf0ac4abc4931779a42162d022917a9c010
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-05 17:41:05 +0900

    Add react-hook-form & zod

--- apps/admin/src/app/auth/signup/page.tsx
+++ apps/admin/src/app/auth/signup/page.tsx
@@ -4,80 +4,79 @@ import { useState } from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import {
-  ToBackButton,
-  Input,
-  SignupTitle,
-  SignupDescription,
-} from 'components';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+import styled from '@emotion/styled';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from 'ui';
+import { z } from 'zod';
 
-import type { StepType } from 'types/signup';
+import { ToBackButton, Input, AuthTitle } from 'admin/components';
 
-const placeholders = {
-  name: '성함을 입력해주세요.',
-  id: '아이디를 입력해주세요.',
-  password: '비밀번호를 입력해주세요.',
-} as const;
+const schema = z.object({
+  name: z
+    .string()
+    .min(2, { message: '성함을 2글자 이상 입력해주세요.' })
+    .max(5, { message: '성함을 5글자 이하로 입력해주세요.' })
+    .regex(/^[가-힣]+$/, { message: '성함은 한글로만 입력해주세요.' }),
+});
 
-const stepArray: StepType[] = ['name', 'id', 'password'];
-
-const nextStep = (step: StepType) => {
-  const currentValueIndex = stepArray.indexOf(step);
-  return stepArray[currentValueIndex + 1];
-};
-
-const previousStep = (step: StepType) => {
-  const currentValueIndex = stepArray.indexOf(step);
-  return stepArray[currentValueIndex - 1];
-};
+type FormValues = z.infer<typeof schema>;
 
 export default function SignupPage() {
-  const [step, setStep] = useState<StepType>('name');
   const [isLoading, setIsLoading] = useState(false);
+
   const { replace } = useRouter();
 
-  const clickToBack = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      step === 'name'
-        ? replace('/auth/signin')
-        : setStep((value) => previousStep(value));
-      setIsLoading(false);
-    }, 1000);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: zodResolver(schema) });
 
-  const clickToNextButton = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
     setIsLoading(true);
-    setTimeout(() => {
-      step === 'password'
-        ? replace('/auth/signup/pending')
-        : setStep((value) => nextStep(value));
-      setIsLoading(false);
-    }, 1000);
+    console.log(data);
+    setTimeout(() => setIsLoading(false), 1000);
+    replace('/auth/signup/pending');
   };
 
   return (
     <>
-      <ToBackButton onClick={clickToBack} />
-      <SignupTitle step={step} />
-      <Input
-        placeholder={placeholders[step]}
-        height='2.75rem'
-        marginTop='2.5rem'
-      />
-      {step === 'password' && (
-        <Input placeholder='비밀번호를 재입력해주세요.' marginTop='1.5rem' />
-      )}
-      <SignupDescription step={step} />
-      <Button
-        onClick={clickToNextButton}
-        position='absolute'
-        bottom='1.5rem'
-        isLoading={isLoading}
-      >
-        다음
-      </Button>
+      <ToBackButton onClick={() => replace('/auth/signin')} />
+      <AuthTitle>
+        우리 학교 선생님인지
+        <br />
+        확인하기 위해 성함을 입력해주세요.
+      </AuthTitle>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          placeholder={'성함을 입력해주세요.'}
+          marginTop='1.5rem'
+          isError={errors.name ? true : false}
+          resetBtn={true}
+          {...register('name')}
+        />
+        {errors.name && (
+          <ErrorMessage>{`* ${errors.name.message}`}</ErrorMessage>
+        )}
+        <Button
+          type='submit'
+          position='absolute'
+          bottom='1.5rem'
+          isLoading={isLoading}
+        >
+          확인
+        </Button>
+      </form>
     </>
   );
 }
+
+const ErrorMessage = styled.p`
+  font-weight: 400;
+  font-size: 12px;
+  margin-top: 1rem;
+  color: #f93535;
+`;
