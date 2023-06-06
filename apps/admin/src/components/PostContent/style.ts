2bdfd5b0a210f206fffc1d243308be14fad97430
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-06-06 13:25:19 +0900

    Update postcontent style.ts

--- apps/admin/src/components/PostContent/style.ts
+++ apps/admin/src/components/PostContent/style.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 export const PostContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  z-index: 1;
 `;
 
 export const Title = styled.h1`
