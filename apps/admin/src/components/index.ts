debc40b98f72d35c4fbaa184a0b3b7801bf51b7b
Lee-Seungje <moondgod@gmail.com>
2023-06-07 14:26:42 +0900

    Delete detailPageHeader

--- apps/admin/src/components/index.ts
+++ apps/admin/src/components/index.ts
@@ -4,7 +4,6 @@ export { default as AuthModel } from './AuthModel';
 export { default as GalleryCard } from './GalleryCard';
 export { default as AuthDescription } from './AuthDescription';
 export { default as Header } from './Header';
-export { default as DetailPageHeader } from './DetailPageHeader';
 export { default as PostContent } from './PostContent';
 export { default as Category } from './Category';
 export { default as PostCard } from './PostCard';
