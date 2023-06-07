eb3fdb09088a8ff50a8ebbe2933f855c3e04ba24
Lee-Seungje <moondgod@gmail.com>
2023-06-07 17:53:49 +0900

    Delete postListHeader style

--- apps/admin/src/components/PostListHeader/style.ts
+++ apps/admin/src/components/PostListHeader/style.ts
@@ -14,35 +14,3 @@ export const CategoryTitle = styled.h1`
   line-height: 1.625rem;
   color: #050505;
 `;
-
-export const AddPostBTN = styled.button`
-  width: 5.3125rem;
-  height: 2.25rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-left: 0.375rem;
-  background: #051b30;
-  border-radius: 1.25rem;
-  cursor: pointer;
-`;
-
-export const AddPostText = styled.p`
-  color: #fcfcfc;
-  padding-left: 0.375rem;
-`;
-
-export const SeeMoreException = styled.div`
-  height: 1.625rem;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-`;
-
-export const SeeMoreText = styled.p`
-  font-weight: 500;
-  font-size: 1rem;
-  line-height: 1.5625rem;
-  color: #505050;
-  margin: 0 0.5rem 0 0.875rem;
-`;
