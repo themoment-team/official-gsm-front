601678be8f48b4bd5463281b8d66d3ca156072bb
Lee-Seungje <moondgod@gmail.com>
2023-06-07 17:59:46 +0900

    Update writeButton icon

--- apps/admin/src/components/PostButton/Write/index.tsx
+++ apps/admin/src/components/PostButton/Write/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import type { LinkProps } from 'next/link';
 
-import { WriteIcon } from 'admin/assets';
+import { PlusIcon } from 'admin/assets';
 import * as S from 'admin/components/PostButton/style';
 
 const WriteButton: React.FC<LinkProps> = ({ ...attributes }) => (
   <S.CustomLink {...attributes}>
-    <WriteIcon color='#ffffff' />
+    <PlusIcon color='#ffffff' />
     <S.Text>글쓰기</S.Text>
   </S.CustomLink>
 );
