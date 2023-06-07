5ad8c6086d59e430e8e385577393f14b9c4a1026
Lee-Seungje <moondgod@gmail.com>
2023-06-07 17:31:52 +0900

    Update px -> rem

--- packages/ui/DateComponent/style.ts
+++ packages/ui/DateComponent/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Date = styled.time`
   font-weight: 400;
-  font-size: 14px;
+  font-size: 0.875rem;
   line-height: 1rem;
   color: #9e9e9e;
   font-feature-settings: 'tnum';
