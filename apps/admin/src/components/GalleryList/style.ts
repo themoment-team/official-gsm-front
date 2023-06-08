e8af4b0a2df2fd673278d1c205c9b7d7e1b498b9
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-08 15:49:47 +0900

    Add height for fixed controller

--- apps/admin/src/components/GalleryList/style.ts
+++ apps/admin/src/components/GalleryList/style.ts
@@ -2,8 +2,9 @@ import styled from '@emotion/styled';
 
 export const CardContainer = styled.div`
   width: 67.5rem;
+  height: 44.875rem;
   margin-top: 1.1875rem;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  gap: 1.6563rem 1rem;
+  gap: 1rem 1.0313rem;
 `;
