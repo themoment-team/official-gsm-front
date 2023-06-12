db7a6905c4a1779d0e28f27a3f2ea92d779ce770
Lee-Seungje <moondgod@gmail.com>
2023-06-12 09:43:49 +0900

    Add useGetPistDetail staleTime, cacheTime

--- packages/api/client/src/hooks/api/post/useGetPostDetail.ts
+++ packages/api/client/src/hooks/api/post/useGetPostDetail.ts
@@ -1,11 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 
-import type { PostDetailType} from 'api/client';
+import type { PostDetailType } from 'api/client';
 import { postUrl, get, postQueryKeys } from 'api/client';
 
 export const useGetPostDetail = (seq: number) => {
-  const query = useQuery<PostDetailType>(postQueryKeys.getPostDetail(seq), () =>
-    get(postUrl.postDetail(seq))
+  const query = useQuery<PostDetailType>(
+    postQueryKeys.getPostDetail(seq),
+    () => get(postUrl.postDetail(seq)),
+    {
+      cacheTime: 1800000,
+      staleTime: 180000,
+    }
   );
 
   return query;
