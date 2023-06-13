6ded537e06d2658e7938258a6fa4fb01d5bf8ca2
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-13 11:59:42 +0900

    Add ThemeProvider

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { Inter } from 'next/font/google';
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] });
+import { ThemeProvider } from '@emotion/react';
+
+import { GlobalStyle } from 'common';
+
+import { theme } from 'client/styles';
 
 export default function RootLayout({
   children,
@@ -10,12 +14,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
       <head>
-        <title>Client</title>
-        <meta name='description' content='client' />
+        <title>광주소프트웨어마이스터고등학교</title>
+        <meta
+          name='description'
+          content='광주소프트웨어마이스터고등학교 공식 홈페이지'
+        />
+        <link
+          rel='stylesheet'
+          as='style'
+          crossOrigin=''
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css'
+        />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
