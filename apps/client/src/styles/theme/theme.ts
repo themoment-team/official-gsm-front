eb7a318ab3518d2af11422f81d269d28ebbcaf37
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-13 11:58:55 +0900

    Add theme

---
+++ apps/client/src/styles/theme/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+  color: {
+    primary: {
+      sky: '#7ACDF4',
+      lime: '#B2E449',
+      navy: '#003365',
+    },
+    sub: {
+      navy: '#051B30',
+      blue: '#3C8AAF',
+      lime: '#C8CE2B',
+      orange: '#FF9877',
+      yellow: '#F8FADB',
+      gray: '#CDD5E2',
+    },
+    gray: {
+      '010': '#FCFCFC',
+      '020': '#FAFAFA',
+      '030': '#F5F5F5',
+      '040': '#EEEEEE',
+      '050': '#E0E0E0',
+      '060': '#9E9E9E',
+      '070': '#616161',
+      '080': '#424242',
+      '090': '#212121',
+    },
+    white: '#FFFFFF',
+    black: '#000000',
+    background: '#F5F9FB',
+  },
+  elevation: {
+    blue: 'rgba(175, 198, 209, 0.2)',
+    black: 'rgba(0, 0, 0, 0.06)',
+  },
+};
