bcb5f022b713d6102ed149a8a9c79079786f4d75
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-13 13:55:04 +0900

    Add example

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,25 @@
 'use client';
 
+import type { Theme } from '@emotion/react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
 import { Button } from 'ui';
 
 export default function Home() {
-  return <Button>button</Button>;
+  return (
+    <>
+      <Button css={exampleCss}>button</Button>
+      <ExampleStyled>styled</ExampleStyled>
+    </>
+  );
 }
+
+const exampleCss = (theme: Theme) => css`
+  background-color: ${theme.color.primary.sky};
+`;
+
+const ExampleStyled = styled.div`
+  ${({ theme }) => theme.typo.h1}
+  background-color: ${({ theme }) => theme.color.primary.lime};
+`;
