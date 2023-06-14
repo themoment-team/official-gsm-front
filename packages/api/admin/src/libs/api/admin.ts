f35530c583771b995d9e5b21b2661a8873249bdd
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-15 08:44:10 +0900

    Resolve eslint error

--- packages/api/admin/src/libs/api/admin.ts
+++ packages/api/admin/src/libs/api/admin.ts
@@ -1,40 +1,17 @@
 import { adminInstance } from './instance';
 
 export const get = async (...args: Parameters<typeof adminInstance.get>) => {
-  try {
-    const { data } = await adminInstance.get(...args);
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const { data } = await adminInstance.get(...args);
+  return data;
 };
 
-export const post = async <T>(
-  ...args: Parameters<typeof adminInstance.post>
-) => {
-  try {
-    await adminInstance.post<T>(...args);
-  } catch (error) {
-    throw error;
-  }
-};
+export const post = async <T>(...args: Parameters<typeof adminInstance.post>) =>
+  await adminInstance.post<T>(...args);
 
 export const patch = async <T>(
   ...args: Parameters<typeof adminInstance.patch>
-) => {
-  try {
-    await adminInstance.patch<T>(...args);
-  } catch (error) {
-    throw error;
-  }
-};
+) => await adminInstance.patch<T>(...args);
 
 export const del = async <T>(
   ...args: Parameters<typeof adminInstance.delete>
-) => {
-  try {
-    await adminInstance.delete<T>(...args);
-  } catch (error) {
-    throw error;
-  }
-};
+) => await adminInstance.delete<T>(...args);
