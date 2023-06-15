ccce0a2fa094818eada8c854944a58737378e30c
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-15 23:36:30 +0900

    Update export sort

--- packages/api/client/src/types/post/index.ts
+++ packages/api/client/src/types/post/index.ts
@@ -1,3 +1,18 @@
+export interface ContentType {
+  postSeq: number;
+  postTitle: string;
+  postWriter: string;
+  createdAt: string;
+  thumbnailUrl?: string;
+  contentPreview: string;
+}
+
+export interface FileInfoType {
+  fileUrl: string;
+  fileName: string;
+  fileExtension: string;
+}
+
 export type PostCategoryType = 'NOTICE' | 'FAMILY_NEWSLETTER' | 'EVENT_GALLERY';
 
 export interface PostDetailType {
@@ -8,22 +23,7 @@ export interface PostDetailType {
   fileInfo: FileInfoType[];
 }
 
-export interface FileInfoType {
-  fileUrl: string;
-  fileName: string;
-  fileExtension: string;
-}
-
 export interface PostListType {
   postList: ContentType[];
   totalPages: number;
-}
-
-export interface ContentType {
-  postSeq: number;
-  postTitle: string;
-  postWriter: string;
-  createdAt: string;
-  thumbnailUrl?: string;
-  contentPreview: string;
 }
