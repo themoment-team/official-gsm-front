134e207d5b130e392963a296a496dc6323cc3bb8
이정우 <comand123@naver.com>
2023-06-15 23:38:08 +0900

    Update FileCard prop

--- apps/admin/src/components/FileCard/index.tsx
+++ apps/admin/src/components/FileCard/index.tsx
@@ -2,23 +2,20 @@ import * as I from 'admin/assets';
 
 import * as S from './style';
 
-
-
 interface FileCardProps {
   fileName?: string;
-  fileLink?: string;
 }
 
 const FileCard: React.FC<FileCardProps> = ({ fileName }) => (
-    <S.FileCardWrap>
-      <S.FileNameWrapper>
-        <I.FileIcon />
-        <S.Filename>{fileName}</S.Filename>
-      </S.FileNameWrapper>
-      <S.CancelButton type='button'>
-        <I.CancelIcon />
-      </S.CancelButton>
-    </S.FileCardWrap>
-  );
+  <S.FileCardWrap>
+    <S.FileNameWrapper>
+      <I.FileIcon />
+      <S.Filename>{fileName}</S.Filename>
+    </S.FileNameWrapper>
+    <S.CancelButton type='button'>
+      <I.CancelIcon />
+    </S.CancelButton>
+  </S.FileCardWrap>
+);
 
 export default FileCard;
