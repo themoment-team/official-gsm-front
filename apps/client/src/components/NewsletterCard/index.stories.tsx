eb449b0962e06ead37ac76e3176ab9cdc37e76ea
Lee-Seungje <moondgod@gmail.com>
2023-06-15 17:09:45 +0900

    Update newsletterCard story

--- apps/client/src/components/NewsletterCard/index.stories.tsx
+++ apps/client/src/components/NewsletterCard/index.stories.tsx
@@ -20,20 +20,5 @@ export const Blue: Story = {
       contentPreview:
         '광주광역시교육청 관내 고등학교 여학생 생리용품 구입 지원금 신...',
     },
-    color: 'blue',
-  },
-};
-
-export const Green: Story = {
-  args: {
-    post: {
-      postSeq: 0,
-      postTitle: '광주광역시 교육청 여학생 생리용품 구입 지원금 신...',
-      postWriter: '정문정',
-      createdAt: '2023-05-24T04:47:01.250197',
-      contentPreview:
-        '광주광역시교육청 관내 고등학교 여학생 생리용품 구입 지원금 신...',
-    },
-    color: 'green',
   },
 };
