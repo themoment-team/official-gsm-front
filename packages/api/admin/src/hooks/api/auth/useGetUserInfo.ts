81f9b68b09777bc2e87fcbbd82b9427fc56404b2
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-17 17:40:51 +0900

    Add useQuery option in hook

--- packages/api/admin/src/hooks/api/auth/useGetUserInfo.ts
+++ packages/api/admin/src/hooks/api/auth/useGetUserInfo.ts
@@ -3,7 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import type { UserInfoType } from 'api/admin';
 import { authQueryKeys, authUrl, get } from 'api/admin';
 
-export const useGetUserInfo = () =>
-  useQuery(authQueryKeys.getUserInfo(), () =>
-    get<UserInfoType>(authUrl.userInfo())
+import type { UseQueryOptions } from '@tanstack/react-query';
+
+export const useGetUserInfo = (options?: UseQueryOptions<UserInfoType>) =>
+  useQuery<UserInfoType>(
+    authQueryKeys.getUserInfo(),
+    () => get(authUrl.userInfo()),
+    options
   );
