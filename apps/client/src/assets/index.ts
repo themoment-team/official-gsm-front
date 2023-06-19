8e3191d324d4e3cc93f854c8b09ab1b79d01df56
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-19 21:39:58 +0900

    Add GSM logos

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -1,3 +1,5 @@
 export { default as DreamIcon } from './introduction/section3/DreamIcon';
 export { default as FutureIcon } from './introduction/section3/FutureIcon';
+export { default as GSMColorLogo } from './svg/GSMColorLogo';
+export { default as GSMWhiteLogo } from './svg/GSMWhiteLogo';
 export { default as ImaginationIcon } from './introduction/section3/ImaginationIcon';
