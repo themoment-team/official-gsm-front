1695bb243486f169d3a3a2921d459996f87b5dd2
이정우 <comand123@naver.com>
2023-06-19 18:15:50 +0900

    Update Add ChevronsIcon

---
+++ apps/client/src/app/assets/introduction/section1/ChevronsIcon.tsx
@@ -0,0 +1,22 @@
+const ChevronsIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+  >
+    <path
+      d='M7.00085 13L12.0009 18L17.0009 13'
+      stroke='#424242'
+      stroke-linecap='round'
+    />
+    <path
+      d='M7.00085 7L12.0009 12L17.0009 7'
+      stroke='#424242'
+      stroke-linecap='round'
+    />
+  </svg>
+);
+
+export default ChevronsIcon;
