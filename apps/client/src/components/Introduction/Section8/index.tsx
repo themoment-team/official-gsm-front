aa1de720225fab58d1838e972dff6b018dcb14f1
예빈 <yebin060310@gmail.com>
2023-06-20 20:00:42 +0900

    Update mou company img

--- apps/client/src/components/Introduction/Section8/index.tsx
+++ apps/client/src/components/Introduction/Section8/index.tsx
@@ -1,67 +1,29 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import Image from 'next/image';
 
 import { IntroductionTitle, SectionTitle, SubTitle } from 'client/components';
 
 import * as S from './style';
 
-const BannerContainer = styled.div`
-  width: 100%;
-  overflow: hidden;
-`;
-
-const BannerList = styled.ul`
-  display: flex;
-  padding: 0;
-  margin: 0;
-  animation: scrollBanner 20s linear infinite;
-  @keyframes scrollBanner {
-    0% {
-      transform: translateX(0);
-    }
-    100% {
-      transform: translateX(-100%);
-    }
-  }
-`;
-
-const BannerItem = styled.li`
-  flex: 0 0 100%;
-  list-style: none;
-  width: 300px;
-  height: 100px;
-`;
-
-const BannerImage = styled.img`
-  width: 100%;
-  height: auto;
-`;
-
 const Section8 = () => {
-  const bannerRef = useRef<HTMLUListElement>(null);
-
   useEffect(() => {
-    const bannerList = bannerRef.current;
-    let animationFrameId: number;
+    const roller = document.querySelector('.rolling-list') as HTMLElement;
+    roller.id = 'roller1';
 
-    const handleAnimation = () => {
-      if (bannerList) {
-        const firstItem = bannerList.firstElementChild;
-        if (firstItem) {
-          bannerList.appendChild(firstItem);
-          bannerList.style.transform = 'translateX(0)';
-        }
-      }
-      animationFrameId = requestAnimationFrame(handleAnimation);
-    };
+    const clone = roller.cloneNode(true) as HTMLElement;
+    clone.id = 'roller2';
+    document.querySelector('.wrap')?.appendChild(clone);
 
-    animationFrameId = requestAnimationFrame(handleAnimation);
+    const roller1 = document.querySelector('#roller1') as HTMLElement;
+    const roller2 = document.querySelector('#roller2') as HTMLElement;
+    roller1.style.left = '0px';
+    roller2.style.left = `${
+      (document.querySelector('.rolling-list ul') as HTMLElement).offsetWidth
+    }px`;
 
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
+    roller.classList.add('original');
+    clone.classList.add('clone');
   }, []);
 
   return (
@@ -74,37 +36,96 @@ const Section8 = () => {
           <SubTitle>꿈˙끼˙업(UP)˙고(GO)</SubTitle>
         </SectionTitle>
       </S.SectionWrap>
-      <S.LogoImgWrap>
-        <S.LogoLoop>
-          <S.LogoBox>
-            <S.Img />
-          </S.LogoBox>
-        </S.LogoLoop>
-      </S.LogoImgWrap>
-      <BannerContainer>
-        <BannerList ref={bannerRef}>
-          <BannerItem>
-            <BannerImage src='/path/to/banner1.jpg' alt='Banner 5' />
-          </BannerItem>
-          <BannerItem
-            css={css`
-              background-color: navy;
-            `}
-          >
-            <BannerImage src='/path/to/banner2.jpg' alt='Banner 2' />
-          </BannerItem>
-          <BannerItem>
-            <BannerImage src='/path/to/banner2.jpg' alt='Banner 2' />
-          </BannerItem>
-          <BannerItem>
-            <BannerImage src='/path/to/banner2.jpg' alt='Banner 2' />
-          </BannerItem>
-          <BannerItem>
-            <BannerImage src='/path/to/banner2.jpg' alt='Banner 2' />
-          </BannerItem>
-          {/* Add more banner items as needed */}
-        </BannerList>
-      </BannerContainer>
+      <div className='wrap'>
+        <div className='rolling-list'>
+          <ul>
+            <li>
+              <div className='image-wrap'>
+                <Image
+                  src='/img/Untitled.png'
+                  alt=''
+                  width={400}
+                  height={200}
+                />
+              </div>
+              <div className='desc'>
+                <strong>First Banner</strong>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                  Neque cumque ratione provident nulla veniam nihil quaerat,
+                  illum officiis hic.
+                  <br />
+                  Laborum in eos possimus, quo ullam nobis nam nemo fuga ipsam?
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className='image-wrap'>
+                <Image
+                  src='/img/Untitled1.png'
+                  alt=''
+                  width={400}
+                  height={200}
+                />
+              </div>
+              <div className='desc'>
+                <strong>Second Banner</strong>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                  Neque cumque ratione provident nulla veniam nihil quaerat,
+                  illum officiis hic.
+                  <br />
+                  Laborum in eos possimus, quo ullam nobis nam nemo fuga ipsam?
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className='image-wrap'>
+                <Image
+                  src='/img/Untitled2.png'
+                  alt=''
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className='desc'>
+                <strong>Third Banner</strong>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                  Neque cumque ratione provident nulla veniam nihil quaerat,
+                  illum officiis hic.
+                  <br />
+                  Laborum in eos possimus, quo ullam nobis nam nemo fuga ipsam?
+                </span>
+              </div>
+            </li>
+            <li>
+              <div className='image-wrap'>
+                <Image
+                  src='/img/Untitled3.png'
+                  alt=''
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className='desc'>
+                <strong>Fourth Banner</strong>
+                <span>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br />
+                  Neque cumque ratione provident nulla veniam nihil quaerat,
+                  illum officiis hic.
+                  <br />
+                  Laborum in eos possimus, quo ullam nobis nam nemo fuga ipsam?
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </S.Layout>
   );
 };
--- apps/client/src/components/Introduction/Section8/style.ts
+++ apps/client/src/components/Introduction/Section8/style.ts
@@ -8,6 +8,59 @@ export const Layout = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+  .wrap {
+    display: flex;
+    overflow: hidden;
+  }
+  .wrap .rolling-list ul {
+    display: flex;
+  }
+  .wrap .rolling-list ul li {
+    width: 589px;
+    height: 276px;
+    box-sizing: border-box;
+    border-radius: 20px;
+    margin: 0 16px;
+    padding: 46px 31px;
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+  }
+  .wrap .rolling-list ul li:nth-child(1) {
+    background-color: #f8e1e3;
+  }
+  .wrap .rolling-list ul li:nth-child(2) {
+    background-color: #ffeab5;
+  }
+  .wrap .rolling-list ul li:nth-child(3) {
+    background-color: #d3e7dc;
+  }
+  .wrap .rolling-list ul li:nth-child(4) {
+    background-color: #a6c1e8;
+  }
+  .wrap .rolling-list ul li:nth-child(even) {
+    margin-top: 80px;
+  }
+  .wrap .rolling-list ul li .image-wrap {
+    padding-right: 32px;
+    width: 156px;
+    flex-shrink: 0;
+  }
+  .wrap .rolling-list ul li .image-wrap > img {
+    width: 100%;
+  }
+  .wrap .rolling-list ul li .desc {
+    font-family: 'BMJUA';
+  }
+  .wrap .rolling-list ul li .desc strong {
+    display: block;
+    font-size: 24px;
+    margin-bottom: 24px;
+  }
+  .wrap .rolling-list ul li .desc span {
+    font-size: 18px;
+    line-height: 1.2;
+  }
 `;
 
 export const SectionWrap = styled.div`
