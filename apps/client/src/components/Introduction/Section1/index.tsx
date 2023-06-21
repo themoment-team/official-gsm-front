09344e3d1bce54ef99e45e47931eee743ff6380c
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-21 16:25:11 +0900

    Remove unused codes

--- apps/client/src/components/Introduction/Section1/index.tsx
+++ apps/client/src/components/Introduction/Section1/index.tsx
@@ -1,55 +1,21 @@
-import { MouseIcon, ChevronsIcon } from 'client/assets';
-import { IntroductionTitle } from 'client/components';
+import styled from '@emotion/styled';
 
-import * as S from './style';
+import CircleDecoration from './CircleDecoration';
+import Scroll from './Scroll';
+import Title from './Title';
 
-const Section1 = () => {
-  const introduceTitle = [
-    {
-      pointsize: '0.8263rem',
-      className: 'firstTitle',
-      title: '상상과 실력으로',
-    },
-    {
-      pointsize: '0.7063rem',
-      className: 'secondTitle',
-      title: 'AI 시대를 선도할',
-    },
-    {
-      pointsize: '0.75rem',
-      className: 'thirdTitle',
-      title: 'SW 마이스터',
-    },
-  ];
+const Section1 = () => (
+  <SectionWrapper>
+    <CircleDecoration />
+    <Title />
+    <Scroll />
+  </SectionWrapper>
+);
 
-  return (
-    <S.Section1Page>
-      <S.Section1PageTitleWrap>
-        <S.GradationCircle />
-        <S.GradationCircle2 />
-        <S.GradationCircle3 />
-        <S.IntroductionTitleWrap>
-          {introduceTitle.map((value) => (
-            <IntroductionTitle
-              key={value.title}
-              pointPosition='bottom'
-              pointColor='lime'
-              pointSize={value.pointsize}
-              className={value.className}
-            >
-              {value.title}
-            </IntroductionTitle>
-          ))}
-        </S.IntroductionTitleWrap>
-        <S.SchoolTitle>GWANGJU SOFTWARE Meister High School</S.SchoolTitle>
-      </S.Section1PageTitleWrap>
-      <S.ScrollContentsWrap>
-        <S.ScrollTitle>scroll</S.ScrollTitle>
-        <MouseIcon />
-        <ChevronsIcon />
-      </S.ScrollContentsWrap>
-    </S.Section1Page>
-  );
-};
+export const SectionWrapper = styled.div`
+  width: 100%;
+  height: 54.375rem;
+  position: relative;
+`;
 
 export default Section1;
