fc46e42095c0ec715af5ddb6b6adef090b65088e
Lee-Seungje <moondgod@gmail.com>
2023-06-22 20:43:05 +0900

    Add donutIcon

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -1,6 +1,6 @@
 export { default as ChevronIcon } from './svg/icon/ChevronIcon';
+export { default as Donut } from './png/icon/Donut.png';
 export { default as DownChevronsIcon } from './introduction/section1/DownChevronsIcon';
 export { default as DreamIcon } from './introduction/section3/DreamIcon';
 export { default as FutureIcon } from './introduction/section3/FutureIcon';
 export { default as ImaginationIcon } from './introduction/section3/ImaginationIcon';
-export { default as MouseIcon } from './introduction/section1/MouseIcon';
