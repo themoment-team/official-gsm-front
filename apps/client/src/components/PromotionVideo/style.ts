f08346d05550dca3da2d3e7dfb38504d7e8185b0
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-23 07:53:58 +0900

    Update PromotionVideo

--- apps/client/src/components/PromotionVideo/style.ts
+++ apps/client/src/components/PromotionVideo/style.ts
@@ -1,8 +1,21 @@
 import styled from '@emotion/styled';
 
-export const Video = styled.video`
+export const VidoeWrapper = styled.div`
   width: 100%;
   position: fixed;
   top: 0;
   z-index: 0;
+`;
+
+export const Video = styled.video`
+  width: 100%;
+`;
+
+export const VideoCover = styled.div`
+  width: 100%;
+  height: calc(56.25vw);
+  background: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  z-index: 2;
 `;
