622b8196f081cd462ba946a2ec2088144738f033
Lee-Seungje <moondgod@gmail.com>
2023-06-22 20:48:49 +0900

    test

--- apps/client/src/components/NoticeBanner/index.stories.tsx
+++ apps/client/src/components/NoticeBanner/index.stories.tsx
@@ -16,7 +16,7 @@ export const Primary: Story = {
       postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
       postWriter: '정문정',
       createdAt: '2023-05-24T04:47:01.250197',
-      contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasd`,
+      contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
     },
   },
 };
