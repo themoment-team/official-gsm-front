fb66bb0621ae265086ddf50179c0a323128c85a2
yoosion030 <yoosioff@gmail.com>
2023-06-24 23:02:08 +0900

    Add Theme, px to rem

--- apps/client/src/components/Introduction/Section6/style.ts
+++ apps/client/src/components/Introduction/Section6/style.ts
@@ -5,9 +5,9 @@ export const Layout = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 180px 340px;
-  gap: 107px;
-  height: 1099px;
+  padding: 180px 21.25rem;
+  gap: 6.6875rem;
+  height: 68.6875rem;
 `;
 
 export const BusinessLayout = styled.div`
@@ -20,49 +20,34 @@ export const BusinessLayout = styled.div`
 export const Business = styled.div`
   display: flex;
   flex-direction: column;
-
   align-items: center;
-  width: 340px;
+  width: 21.25rem;
 `;
 
 export const ImgWrapper = styled.div`
-  width: 340px;
-  height: 340px;
-  margin-bottom: 40px;
+  width: 21.25rem;
+  height: 21.25rem;
+  border-radius: 50%;
+  background-color: #f8f8f8;
+  margin-bottom: 2.5rem;
 `;
 
 export const Title = styled.h2`
+  ${({ theme }) => theme.typo.title};
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 208px;
-  height: 81px;
-
-  background: rgba(122, 205, 244, 0.2);
-  border-radius: 100px;
-
+  width: 13rem;
+  height: 5.0625rem;
+  background-color: rgba(122, 205, 244, 0.2);
+  border-radius: 6.25rem;
   font-weight: 700;
-  font-size: 28px;
-  line-height: 41px;
-  /* identical to box height, or 146% */
-
-  text-align: center;
-
-  /* primary_sky */
-
-  color: #7acdf4;
-  margin-bottom: 16px;
+  color: ${({ theme }) => theme.color.primary['sky']};
+  margin-bottom: 1rem;
 `;
 
 export const SubTitle = styled.p`
+  ${({ theme }) => theme.typo.h3};
   font-weight: 600;
-  font-size: 24px;
-  line-height: 36px;
-  /* identical to box height, or 150% */
-
-  text-align: center;
-
-  /* gray090 */
-
-  color: #212121;
+  color: ${({ theme }) => theme.color.gray['090']};
 `;
