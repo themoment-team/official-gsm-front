47c2be6cd9d0f3d5a881bfcbf353ed1e7617cc2d
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-06-24 22:53:34 +0900

    Update px to rem

--- apps/client/src/components/Introduction/Section4/style.ts
+++ apps/client/src/components/Introduction/Section4/style.ts
@@ -2,15 +2,15 @@ import styled from '@emotion/styled';
 
 export const SectionWrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 1279px;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 export const SectionContentWraper = styled.div`
-  width: 1240px;
-  height: 919px;
+  width: 77.5rem;
+  height: 57.4375rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -26,54 +26,7 @@ export const SectionTitleWrap = styled.div`
 
 export const SectionSubTitle = styled.h3`
   font-weight: 400;
-  font-size: 24px;
-  line-height: 36px;
+  font-size: 1.5rem;
+  line-height: 2.25rem;
   color: #616161;
-`;
-
-export const SectionCardWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 30px;
-`;
-
-export const SmallCard = styled.div<{ bgColor: string }>`
-  width: 495px;
-  height: 347px;
-  border-radius: 30px;
-  position: relative;
-  padding: 48px 0 0 48px;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-export const BigCardTemplate = styled.div<{ bgColor: string }>`
-  width: 715px;
-  height: 347px;
-  border-radius: 30px;
-  position: relative;
-  padding: 48px 0 0 48px;
-  background-color: ${({ bgColor }) => bgColor};
-`;
-
-export const CardImg = styled.div<{ top: string; right: string }>`
-  position: absolute;
-  top: ${({ top }) => top};
-  right: ${({ right }) => right};
-
-  img {
-    object-fit: cover;
-  }
-`;
-
-export const CardTitle = styled.h1<{ color: string }>`
-  font-weight: 700;
-  font-size: 28px;
-  line-height: 41px;
-  color: ${({ color }) => color};
-  position: relative;
-
-  span {
-    color: ${({ color }) => color};
-    opacity: 60%;
-  }
 `;
