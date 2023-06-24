9615f5a819e31fe0688cc4857f51fbd488938625
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-24 22:31:38 +0900

    Update assets index

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -5,5 +5,5 @@ export { default as FooterGSMLogo } from './FooterGSMLogo';
 export { default as FutureIcon } from './introduction/section3/FutureIcon';
 export { default as GSMLogo } from './svg/GSMLogo';
 export { default as ImaginationIcon } from './introduction/section3/ImaginationIcon';
-export { default as UnderscoreIcon } from './svg/icon/UnderscoreIcon';
 export { default as MouseIcon } from './introduction/section1/MouseIcon';
+export { default as UnderscoreIcon } from './svg/icon/UnderscoreIcon';
