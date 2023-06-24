176cafb820ffd12941cd60ef5b98fe3c0aba13db
Lee-Seungje <moondgod@gmail.com>
2023-06-24 22:21:42 +0900

    Update resolve err

--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -4,3 +4,4 @@ export { default as DownChevronsIcon } from './introduction/section1/DownChevron
 export { default as DreamIcon } from './introduction/section3/DreamIcon';
 export { default as FutureIcon } from './introduction/section3/FutureIcon';
 export { default as ImaginationIcon } from './introduction/section3/ImaginationIcon';
+export { default as MouseIcon } from './introduction/section1/MouseIcon';
--- apps/client/src/components/NoticeBanner/style.ts
+++ apps/client/src/components/NoticeBanner/style.ts
@@ -1,5 +1,6 @@
-import styled from '@emotion/styled';
 import Image from 'next/image';
+
+import styled from '@emotion/styled';
 
 export const Banner = styled.div`
   width: 77.5rem;
