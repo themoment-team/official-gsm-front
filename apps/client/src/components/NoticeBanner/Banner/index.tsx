d6c9056735b8ec5625d13131428f831561727f6c
Lee-Seungje <moondgod@gmail.com>
2023-06-25 16:01:35 +0900

    Update relocation donutImage

--- apps/client/src/components/NoticeBanner/Banner/index.tsx
+++ apps/client/src/components/NoticeBanner/Banner/index.tsx
@@ -12,14 +12,14 @@ const Banner: React.FC<BannerProps> = ({
   post: { postTitle, contentPreview },
 }) => (
   <S.Banner>
-    <S.DonutImageWrapper>
-      <Image src='images/notice/Donut.png' alt='donut' fill />
-    </S.DonutImageWrapper>
     <S.TextBox>
       <S.Text>공지사항 📢</S.Text>
       <S.Title>{postTitle}</S.Title>
       <S.Preview>{contentPreview}</S.Preview>
     </S.TextBox>
+    <S.DonutImageWrapper>
+      <Image src='images/notice/Donut.png' alt='donut' fill />
+    </S.DonutImageWrapper>
   </S.Banner>
 );
 
