ba87edc14e234adea8359b428ac9519e438e98ac
Lee-Seungje <moondgod@gmail.com>
2023-06-26 16:08:24 +0900

    Add client galleryCard

---
+++ apps/client/src/components/GalleryCard/style.ts
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+export const GalleryCard = styled(Link)`
+  width: 292px;
+  height: 303px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  cursor: pointer;
+  position: relative;
+  :hover {
+    img {
+      filter: brightness(60%);
+    }
+  }
+`;
+
+export const IMGWrapper = styled.div`
+  width: 292px;
+  height: 180px;
+  border-radius: 20px;
+  overflow: hidden;
+  position: relative;
+  img {
+    object-fit: cover;
+    transition: filter ease-in-out 0.3s;
+  }
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  .text {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    word-break: break-all;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+  }
+`;
+
+export const Title = styled.h5`
+  font-weight: 600;
+  -webkit-line-clamp: 1;
+  ${({ theme }) => theme.typo.h5}
+  color: ${({ theme }) => theme.color.gray['090']};
+`;
+
+export const ContentPreview = styled.p`
+  font-weight: 400;
+  -webkit-line-clamp: 2;
+  ${({ theme }) => theme.typo.body2}
+  color: ${({ theme }) => theme.color.gray['080']};
+`;
