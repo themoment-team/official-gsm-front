703ebe6edffec6e3ffbdb81dc0f12d942dca2a55
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-26 12:19:48 +0900

    Add MainpageNewsletter

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -2,7 +2,14 @@
 
 import styled from '@emotion/styled';
 
-import { Header, PromotionVideo, Footer, SlotMachine } from 'client/components';
+import {
+  Header,
+  PromotionVideo,
+  Footer,
+  SlotMachine,
+  LinkToHelloGSM,
+  MainPageNewsletter,
+} from 'client/components';
 import { useGetWindowScrollHeight, useGetWindowWidth } from 'client/hooks';
 
 export default function Home() {
@@ -21,16 +28,21 @@ export default function Home() {
       <Header segment='' isBackgroundWhite={isBackgroundWhite} />
       <PromotionVideo />
       <SlotMachine />
-      <Content />
+      <Content>
+        <MainPageNewsletter />
+        <LinkToHelloGSM />
+      </Content>
       <Footer />
     </>
   );
 }
 
 const Content = styled.div`
-  height: 2000px;
   position: relative;
-  background-color: white;
+  background-color: ${({ theme }) => theme.color.white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   z-index: 1;
   margin-top: calc(56.25vw - 4rem);
 `;
