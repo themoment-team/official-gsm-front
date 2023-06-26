882e6ce45b4a9d97e2667db69ef6f75cf52d4d82
Lee-Seungje <moondgod@gmail.com>
2023-06-26 16:15:37 +0900

    Update lint

--- apps/client/src/components/GalleryCard/index.stories.tsx
+++ apps/client/src/components/GalleryCard/index.stories.tsx
@@ -1,4 +1,5 @@
 import GalleryCard from '.';
+
 import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
--- apps/client/src/components/GalleryCard/style.ts
+++ apps/client/src/components/GalleryCard/style.ts
@@ -1,5 +1,6 @@
-import styled from '@emotion/styled';
 import Link from 'next/link';
+
+import styled from '@emotion/styled';
 
 export const GalleryCard = styled(Link)`
   width: 292px;
