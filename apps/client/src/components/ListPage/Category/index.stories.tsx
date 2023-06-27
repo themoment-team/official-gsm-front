e3573f8101e9b8c71a66c3ce05ecccb8833377ba
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-27 19:55:17 +0900

    Add ListPageCategory

--- apps/client/src/components/ListPage/Category/index.stories.tsx
+++ apps/client/src/components/ListPage/Category/index.stories.tsx
@@ -5,10 +5,25 @@ import type { Meta, StoryObj } from '@storybook/react';
 export default {
   title: 'client/ListPage/Category',
   component: ListPageCategory,
+  parameters: {},
 } as Meta<typeof ListPageCategory>;
 
 type Story = StoryObj<typeof ListPageCategory>;
 
-export const Primary: Story = {
-  args: {},
+export const Notice: Story = {
+  args: {
+    categoryParam: 'notice',
+  },
+};
+
+export const Newsletter: Story = {
+  args: {
+    categoryParam: 'newsletter',
+  },
+};
+
+export const Gallery: Story = {
+  args: {
+    categoryParam: 'gallery',
+  },
 };
--- apps/client/src/components/ListPage/Category/index.tsx
+++ apps/client/src/components/ListPage/Category/index.tsx
@@ -1,11 +1,31 @@
+import type { CategoryParamsType } from 'client/types';
+
 import CategoryLink from './CategoryLink';
 import * as S from './style';
 
-const ListPageCategory = () => (
+const categoryList = [
+  { href: '/list/notice', category: '공지사항' },
+  { href: '/list/newsletter', category: '가정통신문' },
+  { href: '/list/gallery', category: '행사 갤러리' },
+] as const;
+
+interface ListPageCategoryProps {
+  categoryParam: CategoryParamsType;
+}
+
+const ListPageCategory: React.FC<ListPageCategoryProps> = ({
+  categoryParam,
+}) => (
   <S.ListPageCategory>
-    <CategoryLink />
-    <CategoryLink />
-    <CategoryLink />
+    {categoryList.map(({ href, category }) => (
+      <CategoryLink
+        key={category}
+        href={href}
+        isSelected={href === `/list/${categoryParam}`}
+      >
+        {category}
+      </CategoryLink>
+    ))}
   </S.ListPageCategory>
 );
 
