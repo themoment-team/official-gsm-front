32ed82bac699066bc5653fd8785d4b3e41c1251a
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-27 20:38:19 +0900

    Add PostCard

---
+++ apps/client/src/components/ListPage/PostCard/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './style';
+
+const ListPagePostCard = () => (
+  <S.PostCard>
+    <p />
+  </S.PostCard>
+);
+
+export default ListPagePostCard;
---
+++ apps/client/src/components/ListPage/PostCard/style.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+
+export const PostCard = styled.div`
+  width: 77.5rem;
+  padding: 1rem 1.5rem;
+`;
