946e296218dcdec90a276ebde46e0e10d5954cc7
Lee-Seungje <moondgod@gmail.com>
2023-06-27 20:49:16 +0900

    Add returnChevronIcon

---
+++ apps/client/src/assets/svg/icon/ReturnChevronIcon.tsx
@@ -0,0 +1,19 @@
+const ReturnChevronIcon = () => (
+  <svg
+    width='9'
+    height='14'
+    viewBox='0 0 9 14'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M7.5 1.1665L1.66667 6.99984L7.5 12.8332'
+      stroke='#003365'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+);
+
+export default ReturnChevronIcon;
