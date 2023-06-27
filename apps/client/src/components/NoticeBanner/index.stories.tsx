da6d6daea870411ba200968d29d945e238245678
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-27 15:37:03 +0900

    Update story

--- apps/client/src/components/NoticeBanner/index.stories.tsx
+++ apps/client/src/components/NoticeBanner/index.stories.tsx
@@ -9,44 +9,4 @@ export default {
 
 type Story = StoryObj<typeof NoticeBanner>;
 
-const postList = [
-  {
-    postSeq: 0,
-    postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '정문정',
-    createdAt: '2023-05-24T04:47:01.250197',
-    contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
-  },
-  {
-    postSeq: 1,
-    postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '정문정',
-    createdAt: '2023-05-24T04:47:01.250197',
-    contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
-  },
-  {
-    postSeq: 2,
-    postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '정문정',
-    createdAt: '2023-05-24T04:47:01.250197',
-    contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
-  },
-  {
-    postSeq: 3,
-    postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '정문정',
-    createdAt: '2023-05-24T04:47:01.250197',
-    contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
-  },
-  {
-    postSeq: 4,
-    postTitle: '2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '정문정',
-    createdAt: '2023-05-24T04:47:01.250197',
-    contentPreview: `1. '코로나-19'에 따른 학습공백 최소화를 위하여 한국교육학술정보fdsafasdd`,
-  },
-];
-
-export const Primary: Story = {
-  args: { postList: postList },
-};
+export const Primary: Story = {};
