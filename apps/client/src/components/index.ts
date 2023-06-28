0bd6af649b1e93b5081f9956881376bdf3525457
yoosion030 <yoosioff@gmail.com>
2023-06-28 20:09:45 +0900

    Update Introduction -> About directory name

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,13 +1,13 @@
-export { default as EducationDirection } from './Introduction/Section3/EducationDirection';
+export { default as EducationDirection } from './About/Section3/EducationDirection';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as IntroductionTitle } from './Title/Introduction';
 export { default as NewsTitle } from './Title/News';
 export { default as NewsletterCard } from './NewsletterCard';
 export { default as PromotionVideo } from './PromotionVideo';
-export { default as Section1 } from './Introduction/Section1';
-export { default as Section2 } from './Introduction/Section2';
-export { default as Section3 } from './Introduction/Section3';
+export { default as Section1 } from './About/Section1';
+export { default as Section2 } from './About/Section2';
+export { default as Section3 } from './About/Section3';
 export { default as SectionTitle } from './Title/SectionTitle';
 export { default as SlotMachine } from './SlotMachine';
 export { default as SubTitle } from './Title/SubTitle';
