6579e7ac85474ed39f80e70a998e3fd03a96585c
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-28 16:18:22 +0900

    Remove unused codes

--- packages/api/client/src/types/post/index.ts
+++ packages/api/client/src/types/post/index.ts
@@ -13,13 +13,6 @@ export interface FileInfoType {
   fileExtension: string;
 }
 
-export interface PostBoardInfo {
-  postTitle: string;
-  postContent: string;
-  fileInfo: FileInfoType[];
-  category: PostCategoryType;
-}
-
 export type PostCategoryType = 'NOTICE' | 'FAMILY_NEWSLETTER' | 'EVENT_GALLERY';
 
 export interface PostDetailType {
