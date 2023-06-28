2841b4a23975965a9b5f8c5a8b307a7607a8e342
yoosion030 <yoosioff@gmail.com>
2023-06-28 20:08:55 +0900

    Add AboutPage

---
+++ apps/client/src/app/about/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Section1, Section2, Section3 } from 'client/components';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Section1 />
+      <Section2 />
+      <Section3 />
+    </>
+  );
+}
