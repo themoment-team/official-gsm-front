ee49dc0eaac17152627317e268ca78840fee26dd
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-28 14:29:38 +0900

    Update usePostPostDeta

--- packages/api/admin/src/hooks/api/post/usePostPostData.ts
+++ packages/api/admin/src/hooks/api/post/usePostPostData.ts
@@ -3,7 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { post, postQueryKeys, postUrl } from 'api/admin';
 
 export const usePostPostData = () =>
-  useMutation(postQueryKeys.postPostData(), (postData: any) =>
+  useMutation(postQueryKeys.postPostData(), (postData: FormData) =>
     post(postUrl.writePost(), postData, {
       headers: { 'Content-Type': 'multipart/form-data' },
     })
