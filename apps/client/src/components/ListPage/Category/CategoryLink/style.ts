eb54f229893082c562e6b0e371c53b6287dd4660
hyeongrok7874 <khr5k0228@gmail.com>
2023-06-29 09:01:32 +0900

    Update import CategoryLink style

--- apps/client/src/components/ListPage/Category/CategoryLink/style.ts
+++ apps/client/src/components/ListPage/Category/CategoryLink/style.ts
@@ -1,5 +1,6 @@
-import styled from '@emotion/styled';
 import Link from 'next/link';
+
+import styled from '@emotion/styled';
 
 export const CategoryLink = styled(Link)`
   ${({ theme }) => theme.typo.h5}
