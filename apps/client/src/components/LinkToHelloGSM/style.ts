36b69d9ff3cc8101613484f144a60d2e46ad315f
예빈 <yebin060310@gmail.com>
2023-06-29 11:44:50 +0900

    Add hover animation

--- apps/client/src/components/LinkToHelloGSM/style.ts
+++ apps/client/src/components/LinkToHelloGSM/style.ts
@@ -9,9 +9,34 @@ export const LinkToHelloGSMWrapper = styled(Link)`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: linear-gradient(90deg, #35d391 0%, #6cabe4 48.96%, #c0fbff 100%);
+  background: #35d391;
   border-radius: 1.25rem;
   margin: 7.75rem 0;
+  z-index: 1;
+  position: relative;
+
+  &:after {
+    position: absolute;
+    content: '';
+    width: 0;
+    right: 0;
+    height: inherit;
+    z-index: -1;
+    border-radius: 1.25rem;
+    background: linear-gradient(
+      90deg,
+      #35d391 0%,
+      #6cabe4 48.96%,
+      #c0fbff 100%
+    );
+    box-shadow: inset 0.125rem 0.125rem 0.125rem 0rem rgba(255, 255, 255, 0.5),
+      4px 4px 5px 0px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease-in-out;
+  }
+  &:hover:after {
+    left: 0;
+    width: inherit;
+  }
 `;
 
 export const TextWrapper = styled.div`
