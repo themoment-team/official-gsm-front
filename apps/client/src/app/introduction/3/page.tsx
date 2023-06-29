19e9f99bb009cd7b4e1c896ac643633473c0ad75
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-06-29 13:57:43 +0900

    Update page

--- apps/client/src/app/introduction/3/page.tsx
+++ apps/client/src/app/introduction/3/page.tsx
@@ -2,6 +2,6 @@
 
 import { Section3 } from 'client/components';
 
-export const Section3Page = () => <Section3 />;
+const Section3Page = () => <Section3 />;
 
 export default Section3Page;
--- apps/client/src/app/introduction/4/page.tsx
+++ apps/client/src/app/introduction/4/page.tsx
@@ -2,6 +2,6 @@
 
 import { Section4 } from 'client/components';
 
-export const Section4Page = () => <Section4 />;
+const Section4Page = () => <Section4 />;
 
 export default Section4Page;
