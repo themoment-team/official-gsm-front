12adfb6e0755ac10d951d0f351db4b2c693f22be
Lee-Seungje <moondgod@gmail.com>
2023-06-30 14:04:17 +0900

    Add story

---
+++ apps/admin/src/components/DeleteModal/index.stories.tsx
@@ -0,0 +1,20 @@
+import DeleteModal from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'admin/DeleteModal',
+  component: DeleteModal,
+  parameters: {},
+} as Meta<typeof DeleteModal>;
+
+type Story = StoryObj<typeof DeleteModal>;
+
+export const Primary: Story = {
+  args: {},
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+};
