14290527fca0db022e96f077a9e0784896045119
Lee-Seungje <moondgod@gmail.com>
2023-07-03 23:02:55 +0900

    Delete wrapper

--- apps/admin/src/components/DeleteModal/index.tsx
+++ apps/admin/src/components/DeleteModal/index.tsx
@@ -3,26 +3,24 @@ import { css } from '@emotion/react';
 import * as S from './style';
 
 const DeleteModal = () => (
-  <>
-    <dialog
-      css={css`
-        border-radius: 0.625rem;
-        border: 0;
-        ::backdrop {
-          background: rgba(5, 5, 5, 0.4);
-        }
-      `}
-    >
-      <S.ModalWrapper>
-        <S.DeletePostText>게시물 삭제</S.DeletePostText>
-        <S.ReQuestionText>정말 게시물을 삭제하실건가요?</S.ReQuestionText>
-        <S.ButtonForm method='dialog'>
-          <S.CancleButton>취소하기</S.CancleButton>
-          <S.DeleteButton>삭제하기</S.DeleteButton>
-        </S.ButtonForm>
-      </S.ModalWrapper>
-    </dialog>
-  </>
+  <dialog
+    css={css`
+      border-radius: 0.625rem;
+      border: 0;
+      ::backdrop {
+        background: rgba(5, 5, 5, 0.4);
+      }
+    `}
+  >
+    <S.ModalWrapper>
+      <S.DeletePostText>게시물 삭제</S.DeletePostText>
+      <S.ReQuestionText>정말 게시물을 삭제하실건가요?</S.ReQuestionText>
+      <S.ButtonForm method='dialog'>
+        <S.CancleButton>취소하기</S.CancleButton>
+        <S.DeleteButton>삭제하기</S.DeleteButton>
+      </S.ButtonForm>
+    </S.ModalWrapper>
+  </dialog>
 );
 
 export default DeleteModal;
