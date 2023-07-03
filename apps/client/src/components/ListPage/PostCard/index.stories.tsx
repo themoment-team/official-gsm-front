e0c70fb2889b20ca6654d3ca05c3c25a1de2a529
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-03 19:58:44 +0900

    Update ListPage Card story

--- apps/client/src/components/ListPage/PostCard/index.stories.tsx
+++ apps/client/src/components/ListPage/PostCard/index.stories.tsx
@@ -1,4 +1,4 @@
-import ListPagePostCard from '.';
+import ListPagePostCard, { ListPagePostCardProps } from '.';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -10,12 +10,35 @@ export default {
 
 type Story = StoryObj<typeof ListPagePostCard>;
 
+const args: ListPagePostCardProps = {
+  postIndex: 1,
+  postTitle:
+    '2023학년도 교과서 파일(PDF) 제공 안내 2023학년도 교과서 파일(PDF) 제공 안내 2023학년도 교과서 파일(PDF) 제공 안내',
+  postWriter: '김형록',
+  postDate: '2023-07-03',
+};
+
 export const Primary: Story = {
+  args: { ...args },
+};
+
+export const ThreeDigitIndex: Story = {
   args: {
-    postIndex: 1,
-    postTitle:
-      '2023학년도 교과서 파일(PDF) 제공 안내 2023학년도 교과서 파일(PDF) 제공 안내 2023학년도 교과서 파일(PDF) 제공 안내',
-    postWriter: '김형록',
-    postDate: '2023-07-03',
+    ...args,
+    postIndex: 100,
   },
+};
+
+export const MultipleCard: Story = {
+  render: () => (
+    <>
+      <ListPagePostCard {...args} postIndex={1} />
+      <ListPagePostCard
+        {...args}
+        postIndex={10}
+        postTitle='짧은 제목의 게시글을 위한 값 입니다.'
+      />
+      <ListPagePostCard {...args} postIndex={100} />
+    </>
+  ),
 };
