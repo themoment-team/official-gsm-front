22df413d60a40a94a56457868429dff79cfd24f9
yoosion030 <yoosioff@gmail.com>
2023-07-03 19:55:51 +0900

    Add BusinessCardItemType

--- apps/client/src/types/about/educationDirection.ts
+++ apps/client/src/types/about/educationDirection.ts
@@ -1,11 +1,3 @@
-import type { PointColorType } from './title';
-
-export interface BusinessCardItemType {
-  title: string;
-  subTitle: string;
-  color: PointColorType;
-}
-
 export interface EducationDirectionType {
   icon: React.ReactNode;
   title: React.ReactNode;
