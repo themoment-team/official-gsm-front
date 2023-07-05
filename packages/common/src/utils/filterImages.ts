aa258967c64bb12560f34fd56a991171ce9e4c72
Lee-Seungje <moondgod@gmail.com>
2023-07-05 13:41:17 +0900

    Delete undefined type

--- packages/common/src/utils/filterImages.ts
+++ packages/common/src/utils/filterImages.ts
@@ -2,10 +2,10 @@ import { FileInfoType } from '../../../api/client/src/types/post/index';
 
 const extentions = ['JPG', 'PNG', 'HEIC', 'JPEG', 'WEBP'];
 
-const filterImages = (
-  fileInfo?: FileInfoType[]
-): FileInfoType[] | undefined => {
-  return fileInfo?.filter((file) => extentions.includes(file.fileExtension));
+const filterImages = (fileInfo?: FileInfoType[]): FileInfoType[] => {
+  return (
+    fileInfo?.filter((file) => extentions.includes(file.fileExtension)) ?? []
+  );
 };
 
 export default filterImages;
