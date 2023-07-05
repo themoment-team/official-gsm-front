60867fe8b96aa882872755c8f7699493722010ed
Lee-Seungje <moondgod@gmail.com>
2023-07-05 16:16:50 +0900

    Update fileButton

--- packages/ui/src/components/FileButton/index.tsx
+++ packages/ui/src/components/FileButton/index.tsx
@@ -15,7 +15,7 @@ interface FileButtonProps {
 const FileButton: React.FC<FileButtonProps> = ({
   file: { fileUrl, fileName },
 }) => (
-  <S.FileButton href={fileUrl}>
+  <S.FileButton href={fileUrl} download target='_blank'>
     <FileIcon />
     <S.FileNameText>{fileName}</S.FileNameText>
   </S.FileButton>
--- packages/ui/src/components/FileButton/style.ts
+++ packages/ui/src/components/FileButton/style.ts
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import styled from '@emotion/styled';
 
-export const FileButton = styled(Link)`
+export const FileButton = styled.a`
   display: flex;
   width: fit-content;
   height: 2rem;
