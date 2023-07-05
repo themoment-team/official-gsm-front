ebe9b0e8089a06e4ea3935a7c68cc942905dd4fb
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-05 09:20:44 +0900

    Add postCard in list page

--- apps/client/src/app/list/[category]/page.tsx
+++ apps/client/src/app/list/[category]/page.tsx
@@ -7,20 +7,43 @@ import {
   Header,
   ListPageCategory,
   ListPageTitle,
+  ListPagePostCard,
 } from 'client/components';
 import type { CategoryParamsType } from 'client/types';
+
+import type { PostCategoryType } from 'api/client';
+import { useGetPostList } from 'api/client';
+
+interface PostListQueryStringType {
+  [key: string]: PostCategoryType;
+}
+
+const postListQueryString: PostListQueryStringType = {
+  notice: 'NOTICE',
+  newsletter: 'FAMILY_NEWSLETTER',
+  gallery: 'EVENT_GALLERY',
+} as const;
 
 interface ListPageProps {
   params: { category: CategoryParamsType };
 }
 
 export default function ListPage({ params: { category } }: ListPageProps) {
+  const { data } = useGetPostList(postListQueryString[category], 1);
+
   return (
     <>
       <Header segment={''} />
       <Content>
         <ListPageCategory categoryParam={category} />
         <ListPageTitle category={category} />
+        {data?.postList?.map((post, index) => (
+          <ListPagePostCard
+            key={post.postSeq}
+            postIndex={index + 1}
+            post={post}
+          />
+        ))}
       </Content>
       <Footer />
     </>
