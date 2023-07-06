6cef2f5d17e9b529e691299eb3c38d73216f8f10
Lee-Seungje <moondgod@gmail.com>
2023-07-06 17:50:45 +0900

    Update px -> rem

--- apps/client/src/components/PostContent/style.ts
+++ apps/client/src/components/PostContent/style.ts
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled';
 
 export const ContentWrapper = styled.div`
-  width: 800px;
+  width: 50rem;
   display: flex;
   flex-direction: column;
   gap: 20px;
 `;
 
 export const ImageWrapper = styled.div`
-  width: 800px;
-  height: 486px;
+  width: 50rem;
+  height: 30.375rem;
   overflow: hidden;
   position: relative;
   img {
@@ -21,6 +21,6 @@ export const ContentText = styled.p`
   ${({ theme }) => theme.typo.body1}
   color: ${({ theme }) => theme.color.gray['090']};
   font-weight: 400;
-  width: 800px;
+  width: 50rem;
   word-break: break-all;
 `;
