dcaf3a7715798dce75474aaa6f8cb269a15bfb57
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-06 23:33:59 +0900

    Update FileButton

--- packages/ui/src/components/FileButton/index.stories.tsx
+++ packages/ui/src/components/FileButton/index.stories.tsx
@@ -13,7 +13,8 @@ type Story = StoryObj<typeof FileButton>;
 export const Primary: Story = {
   args: {
     file: {
-      fileUrl: 'https://static-cse.canva.com/blob/649373/createbanners.jpg',
+      fileUrl:
+        'http://bucket.ottokeng.site/d237c2c8-1691-4186-93bd-e8c94d499e28.png',
       fileName: '[2022 서식] 특성화고장학금 지원신청서.pdf',
       fileExtension: 'JPG',
     },
@@ -23,7 +24,8 @@ export const Primary: Story = {
 export const Secondary: Story = {
   args: {
     file: {
-      fileUrl: 'https://static-cse.canva.com/blob/649373/createbanners.jpg',
+      fileUrl:
+        'http://bucket.ottokeng.site/d237c2c8-1691-4186-93bd-e8c94d499e28.png',
       fileName:
         '[2022 서식] 특성화고장학금 지원신청서fdsafdsafdsafsadfsdaffdas.pdf',
       fileExtension: 'JPG',
