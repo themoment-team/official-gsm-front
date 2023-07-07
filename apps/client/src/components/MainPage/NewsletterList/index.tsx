85fa83e3fce7e6e3293f16b453261cdace06269d
Lee-Seungje <moondgod@gmail.com>
2023-07-07 14:51:29 +0900

    Update windowWidth

--- apps/client/src/components/MainPage/NewsletterList/index.tsx
+++ apps/client/src/components/MainPage/NewsletterList/index.tsx
@@ -1,33 +1,24 @@
-import { useEffect, useState } from 'react';
-
 import { CategoryHeader, NewsletterCard } from 'client/components';
+import { useGetWindowWidth } from 'client/hooks';
 
 import { useGetPostList } from 'api/client';
 
 import * as S from './style';
 
+
 const PAGE_SIZE = 3;
 
 const MainpageNewsletterList = () => {
   const { data } = useGetPostList('FAMILY_NEWSLETTER', 1, PAGE_SIZE);
-  const [windowSize, setWindowSize] = useState(0);
-  
-  useEffect(() => {
-    setWindowSize(window.innerWidth);
-    window.addEventListener('resize', () => setWindowSize(window.innerWidth));
-    return () => {
-      window.removeEventListener('resize', () =>
-        setWindowSize(window.innerWidth)
-      );
-    };
-  }, [windowSize]);
+
+  const windowWidth = useGetWindowWidth();
 
   return (
     <S.MainpageNewsletterList>
       <CategoryHeader category={'FAMILY_NEWSLETTER'} />
       <S.NewsletterList>
         {data?.postList?.map((data, index) => {
-          if (windowSize <= 1440) {
+          if (windowWidth <= 1440) {
             return (
               index < 2 && <NewsletterCard key={data.postSeq} post={data} />
             );
