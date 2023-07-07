a8ef3adadd56c7b46c40d369de9b036457a43812
Lee-Seungje <moondgod@gmail.com>
2023-07-07 22:24:24 +0900

    Update newsletter mapping

--- apps/client/src/components/MainPage/NewsletterList/index.tsx
+++ apps/client/src/components/MainPage/NewsletterList/index.tsx
@@ -5,7 +5,6 @@ import { useGetPostList } from 'api/client';
 
 import * as S from './style';
 
-
 const PAGE_SIZE = 3;
 
 const MainpageNewsletterList = () => {
@@ -17,15 +16,9 @@ const MainpageNewsletterList = () => {
     <S.MainpageNewsletterList>
       <CategoryHeader category={'FAMILY_NEWSLETTER'} />
       <S.NewsletterList>
-        {data?.postList?.map((data, index) => {
-          if (windowWidth <= 1440) {
-            return (
-              index < 2 && <NewsletterCard key={data.postSeq} post={data} />
-            );
-          } else {
-            return <NewsletterCard key={data.postSeq} post={data} />;
-          }
-        })}
+        {data?.postList.slice(0, windowWidth <= 1440 ? 2 : 3).map((data) => (
+          <NewsletterCard key={data.postSeq} post={data} />
+        ))}
       </S.NewsletterList>
     </S.MainpageNewsletterList>
   );
