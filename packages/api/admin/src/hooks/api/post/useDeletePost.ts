4b0eaeb5d8f419017df3050abdb7af7a5d5d4058
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-07 14:11:35 +0900

    Remove onSuccess for test

--- packages/api/admin/src/hooks/api/post/useDeletePost.ts
+++ packages/api/admin/src/hooks/api/post/useDeletePost.ts
@@ -1,17 +1,6 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { del, postQueryKeys, postUrl } from 'api/admin';
 
-export const useDeletePost = (seq: number) => {
-  const queryClient = useQueryClient();
-
-  return useMutation(
-    postQueryKeys.deletePost(),
-    () => del(postUrl.deletePost(seq)),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['post', 'list']);
-      },
-    }
-  );
-};
+export const useDeletePost = (seq: number) =>
+  useMutation(postQueryKeys.deletePost(), () => del(postUrl.deletePost(seq)));
