1d727fdd5a1f7b372779245c041927a62da339b2
yoosion030 <yoosioff@gmail.com>
2023-07-08 21:46:53 +0900

    Add section6 responsive style

---
+++ apps/client/src/app/about/6/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Section6 } from 'client/components';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Section6 />
+    </>
+  );
+}
--- apps/client/src/components/About/Section6/style.ts
+++ apps/client/src/components/About/Section6/style.ts
@@ -6,46 +6,16 @@ export const Layout = styled.div`
   justify-content: center;
   align-items: center;
   width: 100vw;
-  height: 68.75rem;
   gap: 6.6875rem;
+
+  padding: 11.25rem 0;
 `;
 
 export const BusinessLayout = styled.div`
   display: flex;
   gap: 6.875rem;
-`;
 
-export const Business = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 21.25rem;
-`;
-
-export const ImgWrapper = styled.div`
-  width: 21.25rem;
-  height: 21.25rem;
-  border-radius: 50%;
-  background-color: #f8f8f8;
-  margin-bottom: 2.5rem;
-`;
-
-export const Title = styled.h2`
-  ${({ theme }) => theme.typo.title};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 13rem;
-  height: 5.0625rem;
-  background-color: rgba(122, 205, 244, 0.2);
-  border-radius: 6.25rem;
-  font-weight: 700;
-  color: ${({ theme }) => theme.color.primary['sky']};
-  margin-bottom: 1rem;
-`;
-
-export const SubTitle = styled.p`
-  ${({ theme }) => theme.typo.h3};
-  font-weight: 600;
-  color: ${({ theme }) => theme.color.gray['090']};
+  @media ${({ theme }) => theme.breakPoint['1024']} {
+    flex-direction: column;
+  }
 `;
