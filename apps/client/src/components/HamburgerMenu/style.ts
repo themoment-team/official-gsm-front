98903d42164f3a90ea624516081a1c8c63240530
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-10 23:26:59 +0900

    Update Hamburger style

--- apps/client/src/components/HamburgerMenu/style.ts
+++ apps/client/src/components/HamburgerMenu/style.ts
@@ -1,6 +1,18 @@
 import styled from '@emotion/styled';
 
-export const HamburgerMenuContainer = styled.menu<{
+export const HamburgerBackground = styled.div<{
+  isHamburgerMenuShow: boolean;
+}>`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  z-index: 0;
+  display: ${({ isHamburgerMenuShow }) =>
+    isHamburgerMenuShow ? 'block' : 'none'};
+`;
+
+export const HamburgerMenu = styled.menu<{
   isHamburgerMenuShow: boolean;
 }>`
   width: 28.75rem;
@@ -8,6 +20,7 @@ export const HamburgerMenuContainer = styled.menu<{
   position: absolute;
   right: 0;
   background: ${({ theme }) => theme.color.white};
+  z-index: 1;
   padding-top: 5rem;
   padding-left: 3.13rem;
   transform: translateX(100%);
@@ -22,6 +35,8 @@ export const HamburgerNav = styled.nav`
   align-items: start;
   gap: 1.5rem;
   color: ${({ theme }) => theme.color.gray['050']};
+  position: relative;
+  z-index: 2;
 
   a {
     ${({ theme }) => theme.typo.h2};
@@ -34,6 +49,7 @@ export const CloseButton = styled.button`
   position: absolute;
   top: 2.5rem;
   right: 2.5rem;
+  z-index: 3;
 
   & > svg {
     display: block;
