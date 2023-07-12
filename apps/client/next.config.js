b00875c3b5f4994405ea9cca260b52a2879d253f
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-13 07:58:43 +0900

    Remove emotion in next config

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['ui', 'common'],
-  compiler: {
-    emotion: true,
-  },
   images: {
     domains: ['bucket.ottokeng.site'],
   },
