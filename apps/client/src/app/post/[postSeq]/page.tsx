13c6df4b4b4a4cdcac83d6590372913a54d92c8c
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-13 12:06:58 +0900

    Add generateMetadata and Remove use client at PostPage

--- apps/client/src/app/post/[postSeq]/page.tsx
+++ apps/client/src/app/post/[postSeq]/page.tsx
@@ -1,8 +1,10 @@
-'use client';
-
-import styled from '@emotion/styled';
-
 import { Footer, Header, AssembledPost } from 'client/components';
+
+import { postUrl } from 'api/client';
+
+import type { PostDetailType } from 'types';
+
+import type { Metadata } from 'next';
 
 interface PostPageProps {
   params: { postSeq: string };
@@ -12,17 +14,25 @@ export default function PostPage({ params: { postSeq } }: PostPageProps) {
   return (
     <>
       <Header segment='list' />
-      <BackGround>
-        <AssembledPost postSeq={parseInt(postSeq)} />
-      </BackGround>
+      <AssembledPost postSeq={parseInt(postSeq)} />
       <Footer />
     </>
   );
 }
 
-const BackGround = styled.div`
-  background: ${({ theme }) => theme.color.background};
-  padding: 5rem 0 7.5rem 0;
-  display: flex;
-  justify-content: center;
-`;
+export const generateMetadata = async ({
+  params,
+}: PostPageProps): Promise<Metadata> => {
+  const postSeq = Number(params.postSeq);
+
+  const post: Promise<PostDetailType> = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/client${postUrl.postDetail(
+      postSeq
+    )}`
+  ).then((res) => res.json());
+
+  return {
+    title: { absolute: (await post).postTitle },
+    description: (await post).postContent,
+  };
+};
