0a9dce90a171bc985b2bc7f0baed056fc334000e
예빈 <yebin060310@gmail.com>
2023-07-13 18:20:58 +0900

    Update MOU img

--- apps/client/src/components/About/Section8/Slide/index.tsx
+++ apps/client/src/components/About/Section8/Slide/index.tsx
@@ -2,39 +2,31 @@ import Image from 'next/image';
 
 import * as S from './style';
 const images = [
-  '/images/about/section8/MOU13.png',
+  '/images/about/section8/MOU.png',
   '/images/about/section8/MOU1.png',
   '/images/about/section8/MOU2.png',
+  '/images/about/section8/MOU3.png',
   '/images/about/section8/MOU4.png',
-  '/images/about/section8/MOU13.png',
-  '/images/about/section8/MOU1.png',
-  '/images/about/section8/MOU2.png',
-  '/images/about/section8/MOU4.png',
+  '/images/about/section8/MOU5.png',
+  '/images/about/section8/MOU6.png',
 ];
 const images2 = [
-  '/images/about/section8/MOU5.png',
-  '/images/about/section8/MOU6.png',
   '/images/about/section8/MOU7.png',
   '/images/about/section8/MOU8.png',
   '/images/about/section8/MOU9.png',
-  '/images/about/section8/MOU5.png',
-  '/images/about/section8/MOU6.png',
-  '/images/about/section8/MOU7.png',
-  '/images/about/section8/MOU8.png',
-  '/images/about/section8/MOU9.png',
+  '/images/about/section8/MOU10.png',
+  '/images/about/section8/MOU11.png',
+  '/images/about/section8/MOU12.png',
+  '/images/about/section8/MOU13.png',
 ];
 const images3 = [
-  '/images/about/section8/MOU11.png',
-  '/images/about/section8/MOU12.png',
-  '/images/about/section8/MOU3.png',
-  '/images/about/section8/MOU13.png',
-  '/images/about/section8/MOU10.png',
-  '/images/about/section8/MOU11.png',
-  '/images/about/section8/MOU12.png',
-  '/images/about/section8/MOU3.png',
-  '/images/about/section8/MOU13.png',
-  '/images/about/section8/MOU10.png',
-  '/images/about/section8/MOU3.png',
+  '/images/about/section8/MOU14.png',
+  '/images/about/section8/MOU15.png',
+  '/images/about/section8/MOU16.png',
+  '/images/about/section8/MOU17.png',
+  '/images/about/section8/MOU18.png',
+  '/images/about/section8/MOU19.png',
+  '/images/about/section8/MOU20.png',
 ];
 
 const Slide = () => (
