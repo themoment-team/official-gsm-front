3a35cee09550530eda42764472ea6921f4c005ad
Lee-Seungje <moondgod@gmail.com>
2023-07-19 10:00:16 +0900

    Update about metaData

--- apps/client/src/app/about/page.tsx
+++ apps/client/src/app/about/page.tsx
@@ -14,7 +14,13 @@ import {
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '학교소개',
+  description: '광주소프트웨어마이스터고등학교 공식 홈페이지 학교 소개입니다.',
+  title: { absolute: '학교 소개' },
+  openGraph: {
+    title: '학교 소개',
+    description:
+      '광주소프트웨어마이스터고등학교 공식 홈페이지 학교 소개입니다.',
+  },
 };
 
 export default function AboutPage() {
