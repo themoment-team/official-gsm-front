98da76cd7363f48e0f543d88cf9cd0dd175f21ff
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-22 14:04:44 +0900

    Remove unused codes

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -6,12 +6,6 @@ import {
   MainPageHeader,
 } from 'client/components';
 
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  description: '광주소프트웨어마이스터고등학교 공식 홈페이지입니다.',
-};
-
 export default function Home() {
   return (
     <>
