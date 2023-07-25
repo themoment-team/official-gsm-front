134424c3115d7068e2eb17e3ace741c95ffbd2a4
hyeongrok7874 <khr5k0228@gmail.com>
2023-07-26 02:35:57 +0900

    Remove use client

--- apps/client/src/components/ListPageContent/Category/index.tsx
+++ apps/client/src/components/ListPageContent/Category/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { CategoryType } from 'types';
 
 import CategoryLink from './CategoryLink';
--- apps/client/src/components/ListPageContent/PostList/PostCard/index.tsx
+++ apps/client/src/components/ListPageContent/PostList/PostCard/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { WriterAndDate } from 'ui';
 
 import type { ContentType } from 'types';
