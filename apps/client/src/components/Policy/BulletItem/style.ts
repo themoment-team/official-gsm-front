d745dfcc522b502188340776a8b29c5c4641ded5
Lee-Seungje <moondgod@gmail.com>
2023-08-08 16:09:50 +0900

    Update px -> rem

--- apps/client/src/components/Policy/BulletItem/style.ts
+++ apps/client/src/components/Policy/BulletItem/style.ts
@@ -2,21 +2,21 @@ import styled from '@emotion/styled';
 
 export const BulletItemWrapper = styled.div`
   display: flex;
-  gap: 12px;
+  gap: 0.75rem;
 `;
 
 export const Bullet = styled.div<{ isDark: boolean }>`
-  width: 10px;
-  height: 10px;
+  width: 0.625rem;
+  height: 0.625rem;
   border-radius: 50%;
   background: #${({ isDark }) => (isDark ? '9E9E9E' : 'b2e449')};
-  margin-top: 9.5px;
+  margin-top: 0.5938rem;
 `;
 
 export const Text = styled.p`
-  width: 1218px;
+  width: 76.125rem;
   color: #616161;
-  font-size: 20px;
+  font-size: 1.25rem;
   font-weight: 400;
-  line-height: 29px;
+  line-height: 1.8125rem;
 `;
