acb3ede3855e9f3d52f8e52af18bed1e40558ea6
hyeongrok7874 <khr5k0228@gmail.com>
2023-08-09 20:12:02 +0900

    Update VideoWrapper

--- apps/client/src/components/PromotionVideo/index.tsx
+++ apps/client/src/components/PromotionVideo/index.tsx
@@ -5,12 +5,12 @@ import { Scroll } from 'client/components';
 import * as S from './style';
 
 const PromotionVideo = () => (
-  <S.VidoeWrapper>
+  <S.VideoWrapper>
     <S.Video src='/video/promotion.mp4' autoPlay loop muted playsInline />
     <S.VideoCover>
       <Scroll isPromotion={true} />
     </S.VideoCover>
-  </S.VidoeWrapper>
+  </S.VideoWrapper>
 );
 
 export default PromotionVideo;
--- apps/client/src/components/PromotionVideo/style.ts
+++ apps/client/src/components/PromotionVideo/style.ts
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
 
-export const VidoeWrapper = styled.div`
+export const VideoWrapper = styled.div`
   width: 100%;
   height: 100vh;
+  height: 100dvh;
   position: fixed;
   top: 0;
   z-index: 0;
@@ -19,6 +20,7 @@ export const Video = styled.video`
 export const VideoCover = styled.div`
   width: 100%;
   height: 100vh;
+  height: 100dvh;
   background: rgba(0, 0, 0, 0.5);
   position: absolute;
   top: 0;
