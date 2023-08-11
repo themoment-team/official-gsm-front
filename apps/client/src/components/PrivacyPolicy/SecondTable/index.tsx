4bef738a8947a62f689afcae43be526b7cd91c01
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-08-11 16:58:44 +0900

    Update edit element

--- apps/client/src/components/PrivacyPolicy/SecondTable/index.tsx
+++ apps/client/src/components/PrivacyPolicy/SecondTable/index.tsx
@@ -14,13 +14,7 @@ const SecondTable = () => (
         이름,학년,반,번호
         <br />※ 회원가입시 수집하는 항목
       </S.TableRowContent>
-      <S.TableRowContent>
-        업체명: ㈜엑스메카
-        <br />
-        주소: 광주광역시 서구 천변좌로 268 KDB생명빌딩 2510호
-        <br />
-        전화번호: 062-374-9986
-      </S.TableRowContent>
+      <S.TableRowContent>더모먼트</S.TableRowContent>
       <S.TableRowContent>방과후 학교 프로그램운영</S.TableRowContent>
       <S.TableRowContent>
         이름,학년,반,번호,학생개인연락처,부 또는 모 성함 및 연락처
