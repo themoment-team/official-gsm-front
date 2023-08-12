bfd05cc99da41d2de065f35657261a5f0d5b4ae5
hyeongrok7874 <khr5k0228@gmail.com>
2023-08-12 23:14:43 +0900

    Remove video for test

--- apps/client/src/components/PromotionVideo/index.tsx
+++ apps/client/src/components/PromotionVideo/index.tsx
@@ -2,12 +2,11 @@
 
 import { Scroll } from 'client/components';
 
-import Video from './Video';
 import * as S from './style';
 
 const PromotionVideo = () => (
   <S.VideoWrapper>
-    <Video />
+    {/* <Video /> */}
     <S.VideoCover>
       <Scroll isPromotion={true} />
     </S.VideoCover>
