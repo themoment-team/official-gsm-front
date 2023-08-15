d71ad4f16cd640eec971148f224496ad6f4cf03d
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-08-15 22:43:10 +0900

    Update add Policy7 component

---
+++ apps/client/src/components/PrivacyPolicy/Policy7/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './style';
+
+const Policy7 = () => (
+  <S.PolicyContentBox>
+    <S.PolicyTitle>제 7조 (처리하는 개인정보의 항목)</S.PolicyTitle>
+    <S.SubTitle>
+      광주소프트웨어마이스터고등학교에서 처리하는 개인정보는 소관업무 및
+      법령에서 정한 최소한의 개인정보만을 처리하며 자세한 사항은 정보주체가
+      확인할 수 있도록 제2조 3항에서 안내하고 있습니다.
+    </S.SubTitle>
+  </S.PolicyContentBox>
+);
+
+export default Policy7;
