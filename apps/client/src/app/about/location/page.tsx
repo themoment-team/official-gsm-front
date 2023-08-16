5358dd0aea4776c128cbb2c3c23a993f10a73bbc
hyeongrok7874 <khr5k0228@gmail.com>
2023-08-17 08:39:29 +0900

    Update LocationPage metadata

--- apps/client/src/app/about/location/page.tsx
+++ apps/client/src/app/about/location/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: '찾아오시는 길',
     description:
-      '광주소프트웨어마이스터고등학교 공식 홈페이지 학교 소개 페이지입니다.',
+      '광주소프트웨어마이스터고등학교 공식 홈페이지 찾아오시는 길 페이지입니다.',
     url: 'https://official.hellogsm.kr/about/location',
   },
 };
