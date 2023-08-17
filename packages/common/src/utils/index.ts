dd3d063b52a4e6e6f2999a46836838d3b45e2855
hyeongrok7874 <khr5k0228@gmail.com>
2023-08-17 09:33:54 +0900

    Split descriptionFormatting

--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -3,3 +3,4 @@ export { default as formatDate } from './formatDate';
 export { default as secondsToMs } from './secondsToMs';
 export { default as pxToRem } from './pxToRem';
 export { default as filterImages } from './filterImages';
+export { default as descriptionFormatting } from './descriptionFormatting';
