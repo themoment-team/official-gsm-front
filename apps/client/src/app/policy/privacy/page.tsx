a908122723731a85c32da95e549c5b3cc344d40e
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2023-08-21 17:57:26 +0900

    Update add metadata

--- apps/client/src/app/policy/privacy/page.tsx
+++ apps/client/src/app/policy/privacy/page.tsx
@@ -1,5 +1,19 @@
 import { PrivacyPolicy, Header, Footer } from 'client/components';
 
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  description:
+    '광주소프트웨어마이스터고등학교 공식 홈페이지 개인정보처리방침 페이지입니다.',
+  title: { absolute: '개인정보처리방침' },
+  openGraph: {
+    title: '개인정보처리방침',
+    description:
+      '광주소프트웨어마이스터고등학교 공식 홈페이지 개인정보처리방침 페이지입니다.',
+    url: 'https://official.hellogsm.kr/policy/privacy',
+  },
+};
+
 export default function Privacy() {
   return (
     <>
