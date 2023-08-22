40273f84145ec680da9016a74f2a9c7568a10e08
hyeongrok7874 <khr5k0228@gmail.com>
2023-08-22 15:44:57 +0900

    Update getPostList QueryKey

--- packages/api/client/src/libs/queryKeys.ts
+++ packages/api/client/src/libs/queryKeys.ts
@@ -1,11 +1,10 @@
 import type { CategoryQueryStringType } from 'types';
 
 export const postQueryKeys = {
-  getPostList: (category: CategoryQueryStringType, pageNumber: number) => [
-    'post',
-    'list',
-    category,
-    pageNumber,
-  ],
+  getPostList: (
+    category: CategoryQueryStringType,
+    pageNumber: number,
+    pageSize: number
+  ) => ['post', 'list', category, pageNumber, pageSize],
   getPostDetail: (postSeq: number) => ['post', 'detail', postSeq],
 } as const;
