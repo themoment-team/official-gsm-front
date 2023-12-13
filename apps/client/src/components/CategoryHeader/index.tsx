5c6760b639b6b85fb108d7552d72f30ab691574b
Lee-Seungje <moondgod@gmail.com>
2023-12-13 23:33:37 +0900

    Update categoryHeader

--- apps/client/src/components/CategoryHeader/index.tsx
+++ apps/client/src/components/CategoryHeader/index.tsx
@@ -6,17 +6,19 @@ import { NewsTitle } from 'client/components';
 import * as S from './style';
 
 interface CategoryHeaderProps {
-  category: 'FAMILY_NEWSLETTER' | 'EVENT_GALLERY';
+  category: 'FAMILY_NEWSLETTER' | 'EVENT_GALLERY' | 'POPUP_ZONE';
 }
 
 const categories = {
   FAMILY_NEWSLETTER: '가정통신문',
   EVENT_GALLERY: '우리 학교 갤러리',
+  POPUP_ZONE: '팝업존',
 } as const;
 
 const categoryHref = {
   FAMILY_NEWSLETTER: '/list/newsletter',
   EVENT_GALLERY: '/list/gallery',
+  POPUP_ZONE: '/',
 } as const;
 
 const CategoryHeader: React.FC<CategoryHeaderProps> = ({ category }) => (
