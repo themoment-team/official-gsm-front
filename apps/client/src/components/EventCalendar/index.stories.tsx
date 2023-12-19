b1c8b618a8613bdf21c16c04c3ac2a7c6ef55875
예빈 <yebin060310@gmail.com>
2023-12-19 09:12:59 +0900

    Add EventCalendar

---
+++ apps/client/src/components/EventCalendar/index.stories.tsx
@@ -0,0 +1,24 @@
+import EventCalendar from '.';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'client/EventCalendar',
+  component: EventCalendar,
+  parameters: {},
+} as Meta<typeof EventCalendar>;
+
+type Story = StoryObj<typeof EventCalendar>;
+
+export const Primary: Story = {
+  args: {
+    event: [
+      '2023 동백제',
+      '학부모 상담주간',
+      '학부모 상담주간',
+      '학부모 상담주간',
+      '학부모 상담주간',
+      '학부모 상담주간',
+    ],
+  },
+};
---
+++ apps/client/src/components/EventCalendar/index.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+
+import { CalendarIcon } from 'client/assets';
+
+import * as S from './style';
+
+interface EventCalendarProps {
+  event: string[];
+}
+
+const EventCalendar: React.FC<EventCalendarProps> = ({ event }) => (
+  <S.EventCalendar>
+    <S.Title>
+      이벤트
+      <CalendarIcon />
+    </S.Title>
+    {event.map((subtitle, index) => (
+      <S.Subtitle key={index}>
+        <S.Point />
+        <p>{subtitle}</p>
+      </S.Subtitle>
+    ))}
+  </S.EventCalendar>
+);
+
+export default EventCalendar;
